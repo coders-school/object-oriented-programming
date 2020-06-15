35cb222f42c3b2b228a195ce551915400f561659
majkel <michal.sobotka.84@gmail.com>
2020-06-15 22:52:07 +0200

    cargo class updated

--- shm/cargo.hpp
+++ shm/cargo.hpp
@@ -1,1 +1,19 @@
 #pragma once
+
+class Cargo {
+public:
+    Cargo& operator+=(const size_t ammount) {
+        ammount_ += ammount;
+        return *this;
+    }
+    Cargo& operator -=(const int ammount) {
+        if (ammount_ - ammount >= 0) {
+            ammount_ -= ammount;
+        }
+        return *this;
+    }
+private:
+    std::string name_;
+    size_t ammount_;
+    double basePrice_;
+};
