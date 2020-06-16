f3d4a471858c976b91862f246f851e9b26bfaf55
Kuba W <winczukjakub@gmail.com>
2020-06-16 23:02:11 +0200

    added constructor in Island

---
+++ shm/island.cpp
@@ -0,0 +1,4 @@
+#include "island.hpp"
+
+Island::Island(int positionX, int positionY)
+    : position_(positionX, positionY) {}
--- shm/island.hpp
+++ shm/island.hpp
@@ -3,6 +3,8 @@
 #include "coordinates.hpp"
 
 class Island {
+public:
+    Island(int positionX, int positionY);
     Coordinates getCoordinates() const { return position_; }
 private:
     Coordinates position_;
