ff7a0ba276e37541606fc056258302936b2c2541
Marcinati <marcin.kapusta.pk@gmail.com>
2020-06-16 19:16:13 +0200

    coordinates added == operator, move constructors to .hpp

--- shm/coordinates.cpp
+++ shm/coordinates.cpp
@@ -1,1 +1,10 @@
 #include "coordinates.hpp"
+
+Coordinates::Coordinates()
+    : positionX_(0)
+    , positionY_(0)
+    {}
+Coordinates::Coordinates(const int positionX, const int positionY)
+    : positionX_(positionX)
+    , positionY_(positionY)
+    {}
--- shm/coordinates.hpp
+++ shm/coordinates.hpp
@@ -1,31 +1,25 @@
 #pragma once
 
+#include "island.hpp"
+
 #include <vector>
 
 class Coordinates
 {
 public:
-    Coordinates()
-        : positionX_(0), positionY_(0)
-    {}
-    Coordinates(int positionX, int positionY)
-        : positionX_(positionX)
-        , positionY_(positionY)
-    {}
-    //operator porownania
-    // OBWÓD ZIEMI = 360° X 60′ => 21 600 NM
-    // 1 NM = 1,852 KM
-    // 21 600 NM = 40 TYS. KM
-    // x 0 - 360
-    // y 0 - 360
+    Coordinates();
+    Coordinates(const int positionX, const int positionY);
 
-    //x  W-180 0 180E
-    //y N180
-    // 0
-    //-180S
+    Coordinates& operator==(std::vector<int> currentPosition) {
+        currentPosition == position_;
+        return *this;
+    };
+
+    int getPositionX() const { return positionX_; }
+    int getPositionY() const { return positionY_; }
+    std::vector<int> position_ = {getPositionX(), getPositionY()};
 
 private:
-    int positionX_;
-    int positionY_;
-    std::vector<int> position_;
+    const int positionX_;
+    const int positionY_;
 };
