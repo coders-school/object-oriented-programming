385985b46fc24b59ba194fefb5218f27ef209bc8
ArturGubala <gubala-artur@wp.pl>
2020-06-17 01:01:28 +0200

    implementation Player class

---
+++ shm/Player.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <algorithm>
+#include <memory>
+#include <numeric>
+#include <string>
+#include <vector>
+
+class Player {
+public:
+    Player(std::unique_ptr<Ship> ship, size_t money) {
+        ship_ = std::move(ship);
+        money_ = money;
+        avaliableSpace_ = countAvailableSpace();
+    };
+
+    size_t getMoney() const { return money_; };
+    size_t getAvaliableSpace() const { return avaliableSpace_; };
+    size_t getSpeed() const { return ship_->getSpeed(); };
+    Cargo* getCargo(size_t index) { return ship_->getCargo(index); };
+
+private:
+    std::unique_ptr<Ship> ship_;
+    size_t money_;
+    size_t avaliableSpace_;
+    size_t countAvailableSpace();
+};
