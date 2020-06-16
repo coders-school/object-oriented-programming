3379372b788f9c89be25d1fc8565cc2ceaeb5024
majkel <michal.sobotka.84@gmail.com>
2020-06-16 22:45:12 +0200

    Constructor size_t, size_t add to Island

--- shm/island.hpp
+++ shm/island.hpp
@@ -1,18 +1,21 @@
 #pragma once
 
+#include <string>
+
 class Coordinates {
 public:
-    Coordinates(int, int);
+    Coordinates(size_t, size_t);
     bool operator==(const Coordinates& pos) const {
         return positionX == pos.positionX && positionY == pos.positionY;
     }
 private:
-    int positionX;
-    int positionY;
+    size_t positionX;
+    size_t positionY;
 };
 
 class Island {
 public:
+    Island(size_t, size_t);
     Coordinates getPosition();
 private:
     Coordinates position_;
