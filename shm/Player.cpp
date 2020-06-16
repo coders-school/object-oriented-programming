38182102cfa6215148016ab42630197b46537be2
ArturGubala <gubala-artur@wp.pl>
2020-06-17 01:08:37 +0200

    implementation for function which count free space for cargo

--- shm/Player.cpp
+++ shm/Player.cpp
@@ -1,1 +1,11 @@
 #include "Player.hpp"
+
+size_t countAvailableSpace() {
+    auto total = std::accumulate(begin(ship_->getAllCargos()), end(ship_->getAllCargos()), 0, [](const auto& cargo) {
+        cargo->getAmount();
+    });
+    if (ship_->getCapacity() <= total) {
+        return 0;
+    }
+    return ship_->getCapacity() - total;
+}
