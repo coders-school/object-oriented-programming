dceca413dba00d6a227a5f6bdf3f692de7330263
kuchichan <pawel.kucharski@hotmail.com>
2020-06-16 18:54:24 +0200

    Splitting into header files

---
+++ shm/ship.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+
+class Ship {
+public:
+    Ship();
+    Ship(int capacity, int maxCrew, int speed, const std::string& name, size_t id);
+    Ship(int maxCrew, int speed, size_t id);
+
+    void setName(const std::string&);
+
+    Ship& operator-=(size_t);
+    Ship& operator+=(size_t num);
+
+    size_t getCapacity() const { return capacity_; }
+    size_t getMaxCrew() const { return maxCrew_; }
+    size_t getSpeed() const { return speed_; }
+    std::string getName() const { return name_; }
+    size_t getId() const { return id_; }
+
+private:
+    size_t capacity_;
+    size_t maxCrew_;
+    size_t crew_;
+    size_t speed_;
+    std::string name_;
+    const size_t id_;
+};
