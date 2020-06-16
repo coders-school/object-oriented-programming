860e544108c902ad079c8be22d4a4aa713e81898
majkel <michal.sobotka.84@gmail.com>
2020-06-17 00:01:00 +0200

    changes in island constructor

--- shm/island.cpp
+++ shm/island.cpp
@@ -4,9 +4,9 @@ Coordinates::Coordinates(size_t posX, size_t posY)
     : positionX(posX), positionY(posY)
 {}
 
-Island::Island(std::size_t posX, std::size_t posY) {
-    Coordinates(posX, posY);
-}
+Island::Island(std::size_t posX, std::size_t posY)
+    : position_(posX, posY)
+{}
 
 Coordinates Island::getPosition() {
     return position_;
