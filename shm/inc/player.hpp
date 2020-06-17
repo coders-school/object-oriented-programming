532bd70ecfbad88a99cb31b92e9f0ecc282fa124
Vicx95 <wiktor.zyla31@gmail.com>
2020-06-17 15:37:52 +0200

    code formatting in player.hpp

--- shm/inc/player.hpp
+++ shm/inc/player.hpp
@@ -1,23 +1,21 @@
-#include "ship.hpp"
 #include <iostream>
 #include <memory>
 
-class Player{
+#include "ship.hpp"
 
-    public:
+class Player {
+public:
+    Player(Ship& ship, size_t money, size_t availableSpace)
+        : ship_(std::make_unique<Ship>(ship)), money_(money), availableSpace_(availableSpace) {}
 
-    Player(Ship& ship, size_t money, size_t availableSpace) : 
-        ship_(std::make_unique<Ship>(ship)), money_(money), availableSpace_(availableSpace){}
-
-    std::unique_ptr<Ship> getShip()  { return std::move(ship_); }
+    std::unique_ptr<Ship> getShip() { return std::move(ship_); }
     size_t getMoney() const { return money_; }
     size_t getavailableSpace() const { return availableSpace_; }
 
     size_t getSpeed() const { return ship_->getSpeed(); }
-    Cargo getCargo(size_t index) const{ return ship_->getCargo(index); }
-    
+    Cargo getCargo(size_t index) const { return ship_->getCargo(index); }
 
-    private:
+private:
     std::unique_ptr<Ship> ship_;
     size_t money_;
     size_t availableSpace_;
