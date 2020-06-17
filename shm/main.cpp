8726a51815f2fd1efdbcb0f2d4b8f9f9c6e9aa91
malinaq3 <malinaq3@gmail.com>
2020-06-18 00:12:31 +0200

    main.cpp test.cpp and CMake files added

---
+++ shm/main.cpp
@@ -0,0 +1,8 @@
+#include"Cargo.hpp"
+#include"Ship.hpp"
+
+#include<iostream>
+
+int main() {
+    std::cout << "SHM Project\n";
+}
---
+++ shm/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "gtest/gtest.h"
+
+#include "Cargo.hpp"
+#include "Ship.hpp"
+
+TEST(SHM, ShouldBuild) {
+    EXPECT_TRUE(true);
+}
