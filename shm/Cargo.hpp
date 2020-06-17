3867408d9793a4e91888afae5f47bd96916497fa
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-06-17 22:26:51 +0200

    getters defined as const functions

--- shm/Cargo.hpp
+++ shm/Cargo.hpp
@@ -5,9 +5,9 @@ public:
     Cargo(const std::string& name, size_t amount, size_t basePrice)
         : name_(name), amount_(amount), basePrice_(basePrice) {}
 
-    std::string getName() { return name_; }
-    size_t getAmount() { return amount_; }
-    size_t getBasePrice() { return basePrice_; }
+    std::string getName() const { return name_; }
+    size_t getAmount() const { return amount_; }
+    size_t getBasePrice() const { return basePrice_; }
 
     Cargo& operator+=(const size_t amount) {
         amount_ += amount;
