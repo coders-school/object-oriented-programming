f44e6a356b8c72e6188a314c5e9d116d17835732
Bartlomiej Gottfried <bargot@o2.pl>
2020-06-17 22:04:46 +0200

    Added empty classes to build/test system

--- shm/main.cpp
+++ shm/main.cpp
@@ -1,6 +1,11 @@
+#include "cargo.hpp"
+#include "coordinates.hpp" 
+#include "island.hpp" 
+#include "map.hpp" 
+#include "ship.hpp"
+
 #include <iostream>
 #include <string>
-
 
 int main() {
     std::cout<<"SHM\n";
