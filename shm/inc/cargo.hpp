a49b966989377ed0f53870b23e6800d4927056b6
Patryk Łapiezo <piezolpiezol@gmail.com>
2020-06-17 13:17:48 +0200

    Piezol zad2 (#13)
    * Getters and constructors for cargo
    
    * pass by ref and then copy
    
    Co-authored-by: Piezol <piezolopiezol@gmail.com>
    Co-authored-by: Andrzej Rafalski <anraf1001@gmail.com>

--- shm/inc/cargo.hpp
+++ shm/inc/cargo.hpp
@@ -4,11 +4,19 @@
 
 class Cargo {
 public:
+    Cargo(const std::string& name, size_t amount, size_t basePrice)
+        : name_(name), amount_(amount), basePrice_(basePrice) {}
+
     Cargo& operator+=(size_t amount);
 
     Cargo& operator-=(size_t amount);
 
+
+    std::string getName() const { return name_; }
+    size_t getAmount() const { return amount_; }
+    size_t getBasePrice() const { return basePrice_; }
     bool operator==(const Cargo& cargoToCheck) const;
+
 
 protected:
     std::string name_;
