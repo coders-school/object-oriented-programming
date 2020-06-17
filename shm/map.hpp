23dcd281dd40da76c821885724d41bdb3c816577
Tomasz Eichler <tomasz.eichler@wp.pl>
2020-06-17 23:45:41 +0200

    Added random position generator

--- shm/map.hpp
+++ shm/map.hpp
@@ -4,6 +4,10 @@
 
 #include "island.hpp"
 
+constexpr int initialAmountOfIsland = 10;
+constexpr int maxX = 10;
+constexpr int maxY = 10;
+
 class Map {
 private:
     std::vector<Island> vecOfIslandOnMap_;
