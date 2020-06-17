9b4b75d3bbf560db844356be873ec248523b4a0a
Nessy90 <nessy0991@gmail.com>
2020-06-17 19:32:05 +0200

    add getters to class Cargo

--- shm/Cargo.hpp
+++ shm/Cargo.hpp
@@ -7,6 +7,10 @@ public:
     Cargo& operator+=(const size_t);
     Cargo& operator-=(const size_t);
 
+    std::string getName() const { return _name; }
+    size_t getAmount() const { return _amount; }
+    size_t getBasePrice() const { return _basePrice; }
+
 private:
     std::string _name;
     size_t _amount;
