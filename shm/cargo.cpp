88d1d5abb9b9ce269563a68ccba3365ed6130143
Tomasz Eichler <tomasz.eichler@wp.pl>
2020-06-18 00:01:58 +0200

    merge

--- shm/cargo.cpp
+++ shm/cargo.cpp
@@ -1,31 +1,38 @@
-#include <iostream>
-#include <string>
 #include "cargo.hpp"
 
-    Cargo::Cargo(std::string name, size_t amount, size_t basePrice)
-        : name_(name)
-        , amount_(amount)
-        , basePrice_(basePrice)
-{}
+#include <iostream>
+#include <string>
 
-    Cargo& Cargo::operator+=(size_t amount) {
-        amount_ += amount;
+Cargo::Cargo(std::string name, size_t amount, size_t basePrice)
+    : name_(name), amount_(amount), basePrice_(basePrice) {}
+
+Cargo& Cargo::operator+=(size_t amount) {
+    amount_ += amount;
+    return *this;
+}
+Cargo& Cargo::operator-=(size_t amount) {
+    if (amount_ < amount) {
+        std::cerr << "Cargo is lower than 0 \n";
         return *this;
     }
-    Cargo& Cargo::operator-=(size_t amount) {
-        if (amount_ < amount) {
-            std::cerr << "Cargo is lower than 0 \n";
-            return *this;
-        }
-        amount_ -= amount;
-        return *this;
-    };
+    amount_ -= amount;
+    return *this;
+};
 
-    bool Cargo::operator==(std::string name) {
-        return name_ == name;
-    };
+bool Cargo::operator==(std::string name) {
+    return name_ == name;
+};
 
-std::string Cargo::GetName() const  { return name_; }
-size_t Cargo::GetAmount() const  { return amount_; }
-size_t Cargo::GetBasePrice() const  { return basePrice_; }
+std::string Cargo::GetName() const {
+    return name_;
+}
+size_t Cargo::GetAmount() const {
+    return amount_;
+}
+size_t Cargo::GetBasePrice() const {
+    return basePrice_;
+}
 
+bool Cargo::operator==(std::string name) {
+    return name_ == name;
+};
