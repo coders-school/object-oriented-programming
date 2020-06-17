86cab53055954189b68d076c419c9f3c2c354817
Nessy90 <nessy0991@gmail.com>
2020-06-17 21:15:00 +0200

    add constructor to class Coordinates

--- shm/Coordinates.hpp
+++ shm/Coordinates.hpp
@@ -2,6 +2,10 @@
 
 class Coordinates {
 public:
+    Coordinates(const int positionX,
+                const int positionY)
+        : _positionX(positionX), _positionY(positionY) {}
+
 private:
     const int _positionX;
     const int _positionY;
