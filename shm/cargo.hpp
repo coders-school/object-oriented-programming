2f5d26f1bc1ea2991d7ff84308b1deceff63b3d0
kasiasieradzka <35103235+kasiasieradzka@users.noreply.github.com>
2020-06-17 21:59:33 +0200

    Update cargo.hpp

--- shm/cargo.hpp
+++ shm/cargo.hpp
@@ -6,16 +6,15 @@ class Cargo {
   private: 
      std::string name_;
      size_t amount_;
-     size_t base_price_;
+     size_t basePrice_;
 
   public: 
      Cargo();
-     Cargo(size_t amount, std::string name, size_t base_price) {}
+     Cargo(size_t amount, std::string name, size_t basePrice) {}
      ~Cargo() {;} 
 
      std::string getName();
      size_t getAmount();
-     size_t getBase_price();
+     size_t getBasePrice();
      bool operator==(Cargo& cargo);
-
 };
