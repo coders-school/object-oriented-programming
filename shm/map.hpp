bc30834b4e2a8ba6fee8cecbf4ccfccdc589f01b
Tomasz Eichler <tomasz.eichler@wp.pl>
2020-06-17 22:53:38 +0200

    formatted

--- shm/map.hpp
+++ shm/map.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <vector>
+
 #include "island.hpp"
 
 class Map {
 private:
     std::vector<Island> vecOfIslandOnMap_;
     Island* currentPosition_;
+
 public:
     Map();
 };
