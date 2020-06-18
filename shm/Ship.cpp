a66aa269062e33f143ddfecb00ec875efea8a176
BreWal <waldemar.brejecki@gmail.com>
2020-06-18 20:22:47 +0200

    Getters and setters moved to hpp files

--- shm/Ship.cpp
+++ shm/Ship.cpp
@@ -16,10 +16,6 @@ Ship::Ship(int capacity, int maxCrew, int speed, const std::string& name, size_t
 Ship::Ship(int maxCrew, int speed, size_t id)
     : Ship(0, maxCrew, speed, "", id) {}
 
-void Ship::setName(const std::string& name) {
-    name_ = name;
-}
-
 Ship& Ship::operator-=(size_t num) {
     if (num > crew_) {
         std::cerr << "Number of papays is lower than 0\n";
@@ -35,20 +31,4 @@ Ship& Ship::operator+=(size_t num) {
     }
     crew_ += num;
     return *this;
-}
-
-size_t Ship::getCapacity() const {
-    return capacity_;
-}
-size_t Ship::getMaxCrew() const {
-    return maxCrew_;
-}
-size_t Ship::getSpeed() const {
-    return speed_;
-}
-std::string Ship::getName() const {
-    return name_;
-}
-size_t Ship::getId() const {
-    return id_;
 }
