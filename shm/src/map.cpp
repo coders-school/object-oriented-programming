818d812d71076e724d4e9ff440ce4e43a1491b87
Andrzej Rafalski <anraf1001@gmail.com>
2020-06-18 18:04:21 +0200

    Deleted redundant )

--- shm/src/map.cpp
+++ shm/src/map.cpp
@@ -7,6 +7,6 @@ Island* Map::getIsland(const Coordinates& coordinate) {
     auto island = std::find_if(islands_.begin(), islands_.end(),
                            [&](const auto& island) {
                                return coordinate == island.getPosition();
-                           }));
+                           });
     return island != std::end(islands_) ? &*island : nullptr;
 }
