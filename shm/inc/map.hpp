5a505ab64c28f21a21340645fb66373a14c2ca76
kari003 <kgebski68@gmail.com>
2020-06-18 15:51:58 +0000

    new function in class map

--- shm/inc/map.hpp
+++ shm/inc/map.hpp
@@ -2,10 +2,14 @@
 
 #include <vector>
 
+#include "coordinates.hpp"
 #include "island.hpp"
 
 class Map {
 private:
     std::vector<Island> islands_;
     Island* currentPosition_;
+
+public:
+    Island* getIsland(const Coordinates&);
 };
--- shm/src/map.cpp
+++ shm/src/map.cpp
@@ -1,1 +1,9 @@
 #include "map.hpp"
+
+#include <algorithm>
+Island* Map::getIsland(const Coordinates& coordinate) {
+    return &*(std::find_if(islands_.begin(), islands_.end(),
+                           [&](auto el) {
+                               return coordinate == el.getPosition();
+                           }));
+}
