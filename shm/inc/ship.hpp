a5cb4a401c1689050f068efc84b1a186b38b9ccf
Andrzej Rafalski <anraf1001@gmail.com>
2020-06-18 13:46:59 +0200

    Resolved problem with double getCargo declaration

--- shm/inc/ship.hpp
+++ shm/inc/ship.hpp
@@ -9,9 +9,9 @@ public:
     Ship()
         : id_(-1) {}
 
-    Ship(int capacity, int maxCrew, int speed, const std::string& name, size_t id)
-        : capacity_(0), maxCrew_(maxCrew), crew_(0), speed_(speed), name_(name), id_(id) {}
-    Ship(int maxCrew, int speed, size_t id)
+    Ship(int capacity, int maxCrew, int speed, const std::string& name, unsigned int id)
+        : capacity_(capacity), maxCrew_(maxCrew), crew_(0), speed_(speed), name_(name), id_(id) {}
+    Ship(int maxCrew, int speed, unsigned int id)
         : Ship(0, maxCrew, speed, "", id) {}
 
     void setName(const std::string& name) { name_ = name; }
@@ -27,8 +27,6 @@ public:
 
     Cargo getCargo(size_t index) const;
     std::vector<Cargo>& getallCargo() { return cargo_; }
-
-    Cargo getCargo(size_t index) const;
 
     size_t getAvailableSpace() const;
 
