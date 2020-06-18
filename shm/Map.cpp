4075ca3c713ea32a41abc904d7f55d4a1d799d0f
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-06-18 14:46:48 +0200

    formating

--- shm/Map.cpp
+++ shm/Map.cpp
@@ -10,13 +10,11 @@ void Map::addIsland(const Island& newIsland) {
     islandsLocations_.push_back(newIsland);
 }
 
-Island* Map::getIsland(const Coordinates& coordinate)
-{
+Island* Map::getIsland(const Coordinates& coordinate) {
     auto island = std::find_if(islandsLocations_.begin(), islandsLocations_.end(),
-    [&coordinate](auto& isld)
-    {
-        return isld.Island::getPosition() == coordinate;
-    });
+                               [&coordinate](auto& isld) {
+                                   return isld.Island::getPosition() == coordinate;
+                               });
 
     return &(*island);
 }
