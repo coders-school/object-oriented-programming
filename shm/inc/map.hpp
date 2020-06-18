a3b0fd7759cbfc9b39d03777e148a428a90d0c08
kari0036 <57415454+kari0036@users.noreply.github.com>
2020-06-18 16:01:18 +0000

    new function in class map (#21)
    * Update map.cpp
    
    * Update map.hpp
    
    * Update map.cpp
    
    * Update map.hpp
    
    * Update map.cpp
    
    * Update shm/src/map.cpp
    
    Co-authored-by: kontrybutor <r.massarnowski@gmail.com>
    
    * Update shm/inc/map.hpp
    
    Co-authored-by: kontrybutor <r.massarnowski@gmail.com>
    
    * new function in class map
    
    Co-authored-by: kontrybutor <r.massarnowski@gmail.com>

--- shm/inc/map.hpp
+++ shm/inc/map.hpp
@@ -2,9 +2,13 @@
 
 #include <vector>
 
+#include "coordinates.hpp"
 #include "island.hpp"
 
 class Map {
+public:
+    Island* getIsland(const Coordinates& coordinates);
+    
 private:
     std::vector<Island> islands_;
     Island* currentPosition_;
--- shm/src/map.cpp
+++ shm/src/map.cpp
@@ -1,1 +1,12 @@
 #include "map.hpp"
+
+#include <algorithm>
+
+Island* Map::getIsland(const Coordinates& coordinate) {
+
+    auto island = std::find_if(islands_.begin(), islands_.end(),
+                           [&](const auto& island) {
+                               return coordinate == island.getPosition();
+                           }));
+    return island != std::end(islands_) ? &*island : nullptr;
+}
