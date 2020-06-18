1282cc011ae10966388efc9096e09ba99a1870f7
MonikaZ <monika.zelechowska@o2.pl>
2020-06-18 10:36:34 +0200

    Add unique_ptr, constructor and getters

--- shm/player.cpp
+++ shm/player.cpp
@@ -1,5 +1,8 @@
 #include "player.hpp"
 
+Player::Player(std::unique_ptr<Ship> ship, size_t money, size_t availableSpace)
+    : ship_(std::move(ship)), money_(money), availableSpace_(availableSpace) {}
+
 Cargo* Player::getCargo(size_t index) const {
     if (index < (ship_->getCargo().size())) {
         return &ship_->getCargo()[index];
--- shm/player.hpp
+++ shm/player.hpp
@@ -1,16 +1,19 @@
 #pragma once
 #include <memory>
 
-#include "cargo.hpp"
 #include "ship.hpp"
 
 class Player {
 public:
+    Player(std::unique_ptr<Ship> ship, size_t money, size_t availableSpace);
+
+    size_t getAvailableSpace_() const { return availableSpace_; }
+    size_t getMoney() const { return money_; }
     size_t getSpeed() const { return ship_->getSpeed(); }
     Cargo* getCargo(size_t index) const;
 
 private:
-    std::shared_ptr<Ship> ship_;
+    std::unique_ptr<Ship> ship_;
     size_t money_;
     size_t availableSpace_;
 };
