96d90a1f47bc1715f403f45510c642838349dd6f
Bartlomiej Gottfried <bargot@o2.pl>
2020-06-18 23:11:11 +0200

    Added getIsland method to Map class

--- shm/main.cpp
+++ shm/main.cpp
@@ -10,6 +10,15 @@
 
 int main() {
     std::cout<<"SHM\n";
+    Map mainMap;
+
+    Coordinates location(1,1);
+
+    if(mainMap.getIsland(location)){
+        std::cout<<"Island found!\n";   
+    }
+
+    
 
 
     return 0;
