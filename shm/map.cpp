54ac471ccae95ddcd6766e0f1ecfe681a6159575
mmichalm <65120204+mmichalm@users.noreply.github.com>
2020-06-18 17:23:34 +0200

    Update shm/map.cpp
    Co-authored-by: Kuba-Wi <62112677+Kuba-Wi@users.noreply.github.com>

--- shm/map.cpp
+++ shm/map.cpp
@@ -8,14 +8,12 @@ Map::Map() {
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> distrib(0, 255);
     for (size_t i = 0; i < 10; ++i) {
-        Island temp(distrib(gen), distrib(gen));
-        if (std::none_of(Islands_.begin(), Islands_.end(), [temp](const Island& islnd) {
+Island temp(distrib(gen), distrib(gen));
+        while(std::any_of(Islands_.begin(), Islands_.end(), [temp](const Island& islnd) {
                 return islnd.getCoordinates() == temp.getCoordinates();
             })) {
-            Islands_.push_back(temp);
-        } else {
-            Island temp2(distrib(gen), distrib(gen));
-            Islands_.push_back(temp2);
+            temp = Island(distrib(gen), distrib(gen));
         }
+        Islands_.push_back(temp);
     }
 }
