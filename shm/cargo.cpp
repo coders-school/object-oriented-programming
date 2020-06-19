8b1061e3316acb730423c40413c02235830b57cc
kasiasieradzka <35103235+kasiasieradzka@users.noreply.github.com>
2020-06-19 16:00:39 +0200

    Fixed cargo.cpp

--- shm/cargo.cpp
+++ shm/cargo.cpp
@@ -1,19 +1,15 @@
 #include "cargo.hpp"
 
-class Cargo {
-  public:
-Cargo();
-Cargo(uint32_t amount, std::string name, uint32_t base_price)
-    : amount_(amount), name_(name), base_price_(base_price)
+Cargo::Cargo();
+Cargo::Cargo(uint32_t amount, std::string name, uint32_t basePrice)
+    : amount_(amount), name_(name), basePrice_(basePrice)
 {}
-~Cargo() {;} 
+~Cargo::Cargo() {;} 
 
-std::string getName() const { return name_; }
-uint32_t getAmount() const { return amount_; }
-uint32_t getBase_price() const { return base_price_; }
+std::string Cargo::getName() const { return name_; }
+uint32_t Cargo::getAmount() const { return amount_; }
+uint32_t Cargo::getBasePrice() const { return basePrice_; }
 
-bool operator == (Cargo& cargo) { 
+bool Cargo::operator == (Cargo& cargo) { 
     return (name_ == cargo.name_) && (amount_ == cargo.amount_) && (base_price_ == cargo.base_price_);
 }
-
-};
