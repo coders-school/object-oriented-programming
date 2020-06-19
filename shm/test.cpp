34b288d33ea48a70484a22424c630b27e4b65ba4
duszek <marcin.dus@gmail.com>
2020-06-19 22:55:42 +0200

    zadanie 2 done

--- shm/test.cpp
+++ shm/test.cpp
@@ -3,16 +3,26 @@
 #include "Cargo.hpp"
 #include "gtest/gtest.h"
 
-TEST(cargo, TwoCargosWithTheSameAmountShouldBeEqual) {
-  Cargo cargoA, cargoB;
-  cargoA += 10;
-  cargoB += 10;
-  ASSERT_EQ(cargoA, cargoB);
+TEST(cargo, TwoCargosWithTheSameAmountShouldBeEqual)
+{
+    Cargo cargoA, cargoB;
+    cargoA += 10;
+    cargoB += 10;
+    ASSERT_EQ(cargoA, cargoB);
 }
 
-TEST(cargo, TwoCargosWithDifferentAmountShouldNotBeEqual) {
-  Cargo cargoA, cargoB;
-  cargoA += 10;
-  cargoB += 100;
-  ASSERT_NE(cargoA, cargoB);
+TEST(cargo, TwoCargosWithDifferentAmountShouldNotBeEqual)
+{
+    Cargo cargoA, cargoB;
+    cargoA += 10;
+    cargoB += 100;
+    ASSERT_NE(cargoA, cargoB);
+}
+
+TEST(cargo, TwoCargosWithDifferentAmountShouldNotBeEqualWithGetters)
+{
+    Cargo cargoA(10, 1), cargoB(100, 2);
+    EXPECT_FALSE(cargoA.getAmount() == cargoB.getAmount());
+    cargoA+=90;
+    EXPECT_EQ(cargoA.getAmount(), cargoB.getAmount());
 }
