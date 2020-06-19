217aa7af8c861ac6d55ed04b3a68daf4ed106b79
Kasperice <kasper@alert.pl>
2020-06-19 07:58:48 +0200

    Added getters to coordinates class

--- shm/coordinates.cpp
+++ shm/coordinates.cpp
@@ -5,3 +5,11 @@ Coordinates::Coordinates(int32_t positionX, int32_t positionY) : positionX_(posi
 bool Coordinates::operator==(const Coordinates& position) {
     return positionX_ == position.positionX_ && positionY_ == position.positionY_;
 }
+
+int32_t Coordinates::getPositionX() const {
+    return positionX_;
+}
+
+int32_t Coordinates::getPositionY() const {
+    return positionY_;
+}
--- shm/coordinates.hpp
+++ shm/coordinates.hpp
@@ -6,8 +6,10 @@ class Coordinates {
 public:
     Coordinates(int32_t positionX, int32_t positionY);
     bool operator==(const Coordinates& position);
+    int32_t getPositionX() const;
+    int32_t getPositionY() const;
 
 private:
-    const int positionX_;
-    const int positionY_;
+    const int32_t positionX_;
+    const int32_t positionY_;
 };
