69e5bd15982cb1da293ae31ea6d096be9fd2cc5d
Marcinati <65040642+Marcinati@users.noreply.github.com>
2020-06-19 14:25:31 +0200

    without vector

--- shm/map.cpp
+++ shm/map.cpp
@@ -8,13 +8,11 @@ Map::Map() {
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> distrib(1, initialAmountOfIsland);
 
-    std::transform(posX.begin(),
-                   posX.end(),
-                   posY.begin(),
-                   std::back_inserter(islandsOnMap_),
-                   [](int x, int y) {
-                       return std::make_pair(x, y);
-                   });
+    for (auto islandCounter = 0; islandCounter < initialAmountOfIsland; ++islandCounter) {
+        int posX = distrib(gen);
+        int posY = distrib(gen);
+        islandsOnMap_.push_back(std::unique_ptr<Island>(posX, posY));
+    }  
 
     Island* getIsland(const Island::Coordinates& desiredCoordinate) {
         std::find_if(islandsOnMap_.begin(), islandsOnMap_.cend(), [desiredCoordinate](const auto& el) {
