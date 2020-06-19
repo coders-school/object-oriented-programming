057662357aba0626f5f5817ab6e28fc14cf6ad1d
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-06-19 18:44:05 +0200

    Cargo class operator== added

---
+++ shm/cargo.cpp
@@ -0,0 +1,20 @@
+#include "cargo.hpp"
+
+bool Cargo::operator==(Cargo& cargo) {
+    return (name_ == cargo.name_) &&
+           (amount_ == cargo.amount_) &&
+           (basePrice_ == cargo.basePrice_);
+}
+bool Cargo::operator!=(Cargo& cargo) {
+    return (name_ != cargo.name_) ||
+           (amount_ != cargo.amount_) ||
+           (basePrice_ != cargo.basePrice_);
+}
+Cargo& Cargo::operator+=(size_t amount) {
+    amount_ += amount;
+    return *this;
+}
+Cargo& Cargo::operator-=(size_t amount) {
+    amount_ -= amount;
+    return *this;
+}
