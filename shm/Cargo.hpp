a839ee7314c925b91a7c48ba315d23aa999f931c
duszek <marcin.dus@gmail.com>
2020-06-19 22:55:42 +0200

    zadanie 2 done

--- shm/Cargo.hpp
+++ shm/Cargo.hpp
@@ -2,31 +2,41 @@
 
 #include <string>
 
-class Cargo {
- public:
-  Cargo& operator+=(size_t amount) {
-    amount_ += amount;
-    return *this;
-  }
-  Cargo& operator-=(size_t amount) {
-    amount_ -= amount;
-    return *this;
-  }
-  Cargo() : amount_() {}
+class Cargo
+{
+public:
+    Cargo& operator+=(size_t amount)
+    {
+        amount_ += amount;
+        return *this;
+    }
+    Cargo& operator-=(size_t amount)
+    {
+        amount_ -= amount;
+        return *this;
+    }
+    Cargo() : amount_(0), basePrice_(0) {}
+    Cargo(size_t amount, size_t basePrice) : amount_(amount), basePrice_(basePrice) {}
 
-  friend bool operator==(const Cargo& lhs, const Cargo& rhs);
-  friend bool operator!=(const Cargo& lhs, const Cargo& rhs);
+    friend bool operator==(const Cargo& lhs, const Cargo& rhs);
+    friend bool operator!=(const Cargo& lhs, const Cargo& rhs);
+    size_t getAmount() { return amount_; }
+    size_t getBasePrice() { return basePrice_; }
 
- protected:
-  std::string name_;
-  size_t amount_;
-  size_t basePrice_;
+
+protected:
+    std::string name_;
+    size_t amount_;
+    size_t basePrice_;
 };
 
-bool operator==(const Cargo& lhs, const Cargo& rhs) {
-  return lhs.amount_ == rhs.amount_;
+bool operator==(const Cargo& lhs, const Cargo& rhs)
+{
+    return lhs.amount_ == rhs.amount_;
 }
 
-bool operator!=(const Cargo& lhs, const Cargo& rhs) {
-  return lhs.amount_ != rhs.amount_;
+bool operator!=(const Cargo& lhs, const Cargo& rhs)
+{
+    return lhs.amount_ != rhs.amount_;
 }
+
