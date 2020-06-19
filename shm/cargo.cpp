a336e354e28332cf445dc05cb464fd50ef80e3d5
kasiasieradzka <35103235+kasiasieradzka@users.noreply.github.com>
2020-06-19 13:37:19 +0200

    Update cargo.cpp
    I applied the changes that you proposed, but I'm not sure if this should look like this

--- shm/cargo.cpp
+++ shm/cargo.cpp
@@ -1,11 +1,13 @@
 #include "cargo.hpp"
 
+class Cargo {
 Cargo();
 Cargo(uint8_t amount, std::string name, uint8_t base_price)
     : amount_(amount), name_(name), base_price_(base_price)
 {}
 ~Cargo() {;} 
 
+  public:
 std::string getName() const { return name_; }
 uint8_t getAmount() const { return amount_; }
 uint8_t getBase_price() const { return base_price_; }
@@ -13,3 +15,5 @@ uint8_t getBase_price() const { return base_price_; }
 bool operator == (Cargo& cargo) { 
     return (name_ == cargo.name_) && (amount_ == cargo.amount_) && (base_price_ == cargo.base_price_);
 }
+
+};
