d503f318997182a35c98f871330950ba86f20ca6
kasiasieradzka <35103235+kasiasieradzka@users.noreply.github.com>
2020-06-19 13:41:20 +0200

    update(3) cargo.cpp

--- shm/cargo.cpp
+++ shm/cargo.cpp
@@ -1,13 +1,13 @@
 #include "cargo.hpp"
 
 class Cargo {
+  public:
 Cargo();
 Cargo(uint32_t amount, std::string name, uint32_t base_price)
     : amount_(amount), name_(name), base_price_(base_price)
 {}
 ~Cargo() {;} 
 
-  public:
 std::string getName() const { return name_; }
 uint32_t getAmount() const { return amount_; }
 uint32_t getBase_price() const { return base_price_; }
