6d8b584ef8518ee97584132baa3f2a16a7d0c915
kszytko <krzysiek.szytko@gmail.com>
2020-06-20 15:47:32 +0200

    Added empty PLayer files

--- shm/Player.cpp
+++ shm/Player.cpp
@@ -1,9 +1,2 @@
 #include "Player.hpp"
 
-size_t Player::getSpeed() const {
-    return this->_ship->getSpeed();
-}
-
-Cargo* Player::getCargo(size_t index) const {
-    return this->_ship->getCargo(index);
-}
