579a7e731cc0795a2040456bb4b48de318a078c9
McKucia <macikucia@onet.pl>
2020-06-20 18:13:42 +0200

    Task 2 McKucia first try, operator ==

--- shm/Cargo.cpp
+++ shm/Cargo.cpp
@@ -1,10 +1,22 @@
 #include "Cargo.hpp"
 
+#include <iostream>
+
 Cargo& Cargo::operator+=(size_t amount) {
     amount_ += amount;
     return *this;
 }
 Cargo& Cargo::operator-=(size_t amount) {
+    if (amount > amount_) {
+        std::cerr << "Amount can't be less than 0!\n";
+        return *this;
+    }
     amount_ -= amount;
     return *this;
+}
+
+bool Cargo::operator==(const Cargo& checkCargo) const {
+    return (name_ == checkCargo.name_ &&
+            amount_ == checkCargo.amount_ &&
+            basePrice_ == checkCargo.basePrice_);
 }
