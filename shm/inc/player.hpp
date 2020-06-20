5a1c978e9ee6eb8934c527c2d62850ae1cec8619
Piezol <piezolopiezol@gmail.com>
2020-06-20 18:41:02 +0200

    Vectors should store shared_ptrs of cargo instead of cargo

--- shm/inc/player.hpp
+++ shm/inc/player.hpp
@@ -12,7 +12,7 @@ public:
     size_t getMoney() const { return money_; }
     size_t getavailableSpace() const { return availableSpace_; }
     size_t getSpeed() const { return ship_->getSpeed(); }
-    Cargo* getCargo(size_t index) const { return ship_->getCargo(index); }
+    std::shared_ptr<Cargo> getCargo(size_t index) const { return ship_->getCargo(index); }
 
 private:
     size_t getAvailableSpace() { return ship_->getAvailableSpace(); }
