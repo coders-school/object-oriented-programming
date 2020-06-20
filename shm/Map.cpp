721b39e8b9b20e498bccff4e0b1d58471b8820e0
lisie-sprawy <magda.jankowska6@tlen.pl>
2020-06-20 13:35:55 +0200

    first implementation; compilation error to check

--- shm/Map.cpp
+++ shm/Map.cpp
@@ -1,4 +1,30 @@
 #include "Map.hpp"
+#include <algorithm>
+#include <random>
+#include <iostream>
+
+Map::Map(){
+    islandsLocations_.reserve(amountIslands);
+    generateIslands();
+}
+
+void Map::generateIslands() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(widthIsland, heightIsland);
+    
+      for (size_t i = 0; i < amountIslands; i++) { 
+        size_t positionX; 
+        size_t positionY; 
+        do { 
+            size_t positionX = distrib(gen); 
+            size_t positionY = distrib(gen);
+        } while (std::any_of(islandsLocations_.begin(), islandsLocations_.end(), [positionX, positionY](auto& island) { 
+            return island.getPosition() == Coordinates(positionX, positionY); 
+        })); 
+        islandsLocations_.push_back(Island(positionX, positionY)); 
+    }
+}
 
 void Map::setCurrentPosition(Island* const currentPosition) {
     currentPosition_ = currentPosition;
--- shm/Map.hpp
+++ shm/Map.hpp
@@ -3,11 +3,17 @@
 #include "Island.hpp"
 #include <vector>
 
+constexpr size_t amountIslands = 10;
+constexpr size_t heightIsland = 10;
+constexpr size_t widthIsland = 10;
+
 class Map {
 private:
+    void generateIslands();
     Island* currentPosition_ {nullptr};
     std::vector<Island> islandsLocations_;
 public:
+    Map();
     void setCurrentPosition(Island* const currentPosition);
     void addIsland(const Island& newIsland);
 };
