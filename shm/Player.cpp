c25f4e302d5fee2e9ee8c331373db09c5557bdbd
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-06-20 19:51:36 +0200

    Player class refactored

--- shm/Player.cpp
+++ shm/Player.cpp
@@ -2,7 +2,13 @@
 
 #include <numeric>
 
-size_t countAvailableSpace() {
+Player::Player(std::unique_ptr<Ship> ship, size_t money) {
+    ship_ = std::move(ship);
+    money_ = money;
+    avaliableSpace_ = countAvailableSpace();
+};
+
+size_t Player::countAvailableSpace() {
     auto occupiedSpace = std::accumulate(begin(ship_->getAllCargos()), end(ship_->getAllCargos()), 0, [](const auto& cargo) {
         cargo.getAmount();
     });
