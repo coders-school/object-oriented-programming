67fce4859d1ee7e1bdd134bd7b2e6125ca5b41fa
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-06-20 19:41:42 +0200

    formatting - empty lines

--- shm/Map.hpp
+++ shm/Map.hpp
@@ -7,9 +7,9 @@ class Map {
 private:
     Island* currentPosition_ {nullptr};
     std::vector<Island> islandsLocations_;
+    
 public:
     void setCurrentPosition(Island* const currentPosition);
     void addIsland(const Island& newIsland);
     Island* getIsland(const Coordinates& coordinate);
 };
-
