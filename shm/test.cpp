c19dc0be2efbab8ad26b85f46f4244ac2f01531d
duszek <marcin.dus@gmail.com>
2020-06-21 21:53:08 +0200

    test for map generation added

--- shm/test.cpp
+++ shm/test.cpp
@@ -54,3 +54,20 @@ TEST(player, newMoneyAmountWasSet)
     player.setAvailableSpace(money);
     EXPECT_EQ(player.getAvailableSpace(), money);
 }
+
+TEST(Map, mapWithOneIsland)
+{
+    Map map_(1,1,1);
+    Island island_{Island(0,0)};
+    EXPECT_EQ(map_.getIslands().at(0).getPosition(), island_.getPosition());
+}
+
+
+TEST(Map, mapWith10Island)
+{
+    Map map_;
+    std::vector<Island> is = map_.getIslands();
+    EXPECT_EQ(map_.getIslands().size(), 10);
+    for(auto i : is) std::cout << i << "\n";
+}
+
