558d487bc3754420b0864f323a95418ebf3c0068
duszek <marcin.dus@gmail.com>
2020-06-21 21:52:13 +0200

    compilation fixed, strange error when more islands than size

--- shm/Map.hpp
+++ shm/Map.hpp
@@ -3,36 +3,49 @@
 #include <random>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 #include "Island.hpp"
-
-constexpr size_t MAX_X = 32;
-constexpr size_t MAX_Y = 32;
-
-struct IslandGenerator
-{
-    IslandGenerator() {}
-    IslandGenerator(Coordinates start, Coordinates end) {}
-
-    Island getRandomIsland()
-    {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<> distrib(0, MAX_Y);
-        return Island(distrib(gen), distrib(gen));
-    }
-    void generate(std::vector<Island>& islands, int amount);
-
-    islands.reserve(amount);
-    std::generate(std::begin(islands), std::end(islands), getRandomIsland);
-};
 
 class Map
 {
 
 public:
-    Map() {}
+    Map() : mapXsize(10), mapYsize(10) { randomIslandGenerator(20); }
+    Map(int islandsAmount) { randomIslandGenerator(islandsAmount); }
+    Map(int sizeY, int sizeX, int islandsAmount) : mapXsize(sizeY), mapYsize(sizeX)
+    {
+        randomIslandGenerator(islandsAmount);
+    }
+
+    std::vector<Island> getIslands() { return islands; }
 
 private:
+
+    //std::vector<int> buildCoordinates(
+    
+
+    void randomIslandGenerator(int amount)
+    {
+        static std::random_device rd;
+        static std::mt19937 gen(rd());
+        
+
+        std::vector<int> x(mapXsize);
+        std::iota(std::begin(x), std::end(x), 0);
+        std::shuffle(std::begin(x), std::end(x), gen);
+
+        std::vector<int> y(mapYsize);
+        std::iota(std::begin(y), std::end(y), 0);
+        std::shuffle(std::begin(y), std::end(y), gen);
+
+        for (int i = 0; i < amount; i++)
+            {
+            islands.push_back(Island(x[i], y[i]));
+            }
+    }
+
+    size_t mapXsize;
+    size_t mapYsize;
     std::vector<Island> islands;
     Island* currentPosition_;
 };
