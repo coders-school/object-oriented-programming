9f4198952b6531352fab5148784aec5039bce49d
Adam Mierzwiak <5563693+adamvm@users.noreply.github.com>
2020-06-21 08:58:00 +0200

    Add Player class (Task7)

---
+++ shm/Player.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <memory>
+#include "Ship.hpp"
+
+class Player
+{
+public:
+    Player(Ship ship, size_t money, size_t space) : money_(money), availableSpace_(space)
+    {
+        ship_ = std::make_unique<Ship>(std::move(ship));
+    };
+
+    [[nodiscard]] size_t getMoney() const { return money_; }
+    [[nodiscard]] size_t getAvailableSpace() const { return availableSpace_; }
+
+    void setMoney(size_t amount) { money_ = amount; }
+    void setAvailableSpace(size_t space) { availableSpace_ = space; }
+
+private:
+    std::unique_ptr<Ship> ship_;
+    size_t money_;
+    size_t availableSpace_;
+};
