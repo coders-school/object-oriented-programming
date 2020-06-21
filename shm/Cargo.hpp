3c6109417aacb373753c49497a318bfd51edefd3
McKucia <macikucia@onet.pl>
2020-06-21 22:32:14 +0200

    Adding constructor & getters

--- shm/Cargo.hpp
+++ shm/Cargo.hpp
@@ -4,6 +4,13 @@
 
 class Cargo {
 public:
+    Cargo(std::string n = "", size_t a = 0, size_t bp = 0):
+        name_(n), amount_(a), basePrice_(bp) {};
+    
+    std::string getName() const { return name_; }
+    size_t getAmount() const { return amount_; }
+    size_t getBasePrice() const { return basePrice_; }
+
     Cargo& operator+=(size_t amount);
     Cargo& operator-=(size_t amount);
     bool operator==(const Cargo& checkCargo) const;
