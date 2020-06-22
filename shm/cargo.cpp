c0a7efb76cae5b73c7190bb0a0eaa1882c61a382
Marcinati <65040642+Marcinati@users.noreply.github.com>
2020-06-22 18:24:57 +0200

    better ==

--- shm/cargo.cpp
+++ shm/cargo.cpp
@@ -14,16 +14,16 @@ Cargo& Cargo::operator-=(size_t amount) {
     return *this;
 }
 
-bool Cargo::operator==(std::string name) {
-    return name_ == name;
+bool Cargo::operator==(const Cargo& cargo) const {
+    return Cargo.getBasePrice() == basePrice_ && Cargo.getName() == name_;
 }
 
-std::string Cargo::GetName() const {
+std::string Cargo::getName() const {
     return name_;
 }
-size_t Cargo::GetAmount() const {
+size_t Cargo::getAmount() const {
     return amount_;
 }
-size_t Cargo::GetBasePrice() const {
+size_t Cargo::getBasePrice() const {
     return basePrice_;
 }
