6b37f2d6af9a92a9d48ffbe77b64c8c247cfee67
Marcinati <65040642+Marcinati@users.noreply.github.com>
2020-06-22 18:44:37 +0200

    +default_position

--- shm/coordinates.cpp
+++ shm/coordinates.cpp
@@ -1,11 +1,11 @@
 #include "coordinates.hpp"
 
 Coordinates::Coordinates()
-    : positionX_(0), positionY_(0) {}
-    
-Coordinates::Coordinates(const int positionX, const int positionY)
+    : positionX_(DEFAULT_POSITION_X), positionY_(DEFAULT_POSITION_Y) {}
+   
+Coordinates::Coordinates(int positionX, int positionY)
     : positionX_(positionX), positionY_(positionY) {}
 
-bool Coordinates::operator==(const Coordinates& currentPosition) {
-    return positionX_ == currentPosition.positionX && positionY_ == currentPosition.positionY;
+bool Coordinates::operator==(const Coordinates& currentPosition) const {
+    return positionX_ == currentPosition.positionX_ && positionY_ == currentPosition.positionY_;
 }
