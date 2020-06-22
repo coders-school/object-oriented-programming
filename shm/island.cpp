db9fb5692d1dd7c0e257ce6e3299ff4a6c49c7ae
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-06-22 20:25:41 +0200

    Class simplified (setPosition removed); string header removed

--- shm/island.cpp
+++ shm/island.cpp
@@ -4,7 +4,4 @@ Island::Island(size_t posX, size_t posY)
     : position_(posX, posY)
 {}
 
-void Island::setPosition(size_t posX, size_t posY) {
-    position_.setPosition(posX, posY);
-}
 Coordinates Island::getPosition() const { return position_; }
--- shm/island.hpp
+++ shm/island.hpp
@@ -2,13 +2,10 @@
 
 #include "coordinates.hpp"
 
-#include <string>
-
 class Island {
 public:
     Island(size_t posX, size_t posY);
 
-    void setPosition(size_t posX, size_t posY);
     Coordinates getPosition() const;
 
 private:
