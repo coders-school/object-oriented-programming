ef173d3d2924ca2e58de9292006dc17f517439bb
Marcinati <65040642+Marcinati@users.noreply.github.com>
2020-06-22 19:20:36 +0200

    more random generator

--- shm/map.cpp
+++ shm/map.cpp
@@ -6,13 +6,21 @@
 Map::Map() {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> distrib(1, initialAmountOfIsland);
+    std::uniform_int_distribution<> xesGen(0, maxX);
+    std::uniform_int_distribution<> yesGen(0, maxY);
 
     for (auto islandCounter = 0; islandCounter < initialAmountOfIsland; ++islandCounter) {
-        int posX = distrib(gen);
-        int posY = distrib(gen);
-        islandsOnMap_.push_back(std::unique_ptr<Island>(posX, posY));
-    }  
+        while (true) {
+            int posX = xesGen(gen);
+            int posY = yesGen(gen);
+            if (std::none_of(islandsOnMap_.cbegin(), islandsOnMap_.cend(), [posX, posY](const auto& position) {
+                                     return position.first == posX && position.second == posY;
+                                     })) {           
+                islandsOnMap_.push_back(std::unique_ptr<Island>(posX, posY));
+                break;
+            }
+        }
+    }
 
     Island* getIsland(const Island::Coordinates& desiredCoordinate) {
         auto iCoordinate = std::find_if(islandsOnMap_.begin(), islandsOnMap_.end(), [desiredCoordinate](const auto& el) {
@@ -21,6 +29,6 @@ Map::Map() {
         if (iCoordinate != islandsOnMap.end()) {
             return iCoordinate;
         }
-        return 0;
+        return nullptr;
     }
 };
