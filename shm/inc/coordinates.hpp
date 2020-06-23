d42015bf7778ac1a0a3d5e148d3c7d6b2b6608c6
Patryk Łapiezo <piezolpiezol@gmail.com>
2020-06-23 13:29:57 +0200

    Wyklad 3 zadanie 4 (#50)
    * Static distance calculation
    
    * Misspelled function name
    
    Co-authored-by: Piezol <piezolopiezol@gmail.com>

--- shm/inc/coordinates.hpp
+++ shm/inc/coordinates.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <cstddef>
+
 class Coordinates {
 public:
     Coordinates(int positionX, int positionY)
         : positionX_(positionX), positionY_(positionY) {}
 
     bool operator==(const Coordinates& coordToCheck) const;
+
+    static size_t distance(const Coordinates& lhs, const Coordinates& rhs);
 
 private:
     int positionX_;
--- shm/src/coordinates.cpp
+++ shm/src/coordinates.cpp
@@ -1,7 +1,15 @@
 #include "coordinates.hpp"
 
-bool Coordinates::operator==(const Coordinates& coordToCheck) const {
+#include <cmath>
 
+bool Coordinates::operator==(const Coordinates& coordToCheck) const {
     return positionX_ == coordToCheck.positionX_ &&
            positionY_ == coordToCheck.positionY_;
+}
+
+size_t Coordinates::distance(const Coordinates& lhs, const Coordinates& rhs) {
+    return static_cast<size_t>(
+        std::floor(std::sqrt(
+            std::pow(lhs.positionX_ - rhs.positionX_, 2) +
+            std::pow(lhs.positionY_ - rhs.positionY_, 2))));
 }
