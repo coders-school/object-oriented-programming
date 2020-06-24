a2460df21a5cc4df8e68ce9de67ddbaa02c17d21
Marcinati <65040642+Marcinati@users.noreply.github.com>
2020-06-24 11:40:24 +0200

    make virtual

--- shm/cargo.hpp
+++ shm/cargo.hpp
@@ -4,22 +4,18 @@
 
 class Cargo {
 public:
+    Cargo(std::string name, size_t amount, size_t basePrice);
+    
+    virtual Cargo& operator+=(size_t amount);
+    virtual Cargo& operator-=(size_t amount);
+    virtual bool operator==(const Cargo& cargo) const; 
+    
     virtual size_t getPrice() const = 0;
     virtual std::string getName() const = 0;
     virtual size_t getAmount() const = 0;
     virtual size_t getBasePrice() const = 0;
 
-    Cargo(std::string name, size_t amount, size_t basePrice);
-    
-    Cargo& operator+=(size_t amount);
-    Cargo& operator-=(size_t amount);
-    bool operator==(const Cargo& cargo) const; 
-    
-    std::string getName() const;
-    size_t getAmount() const;
-    size_t getBasePrice() const;
-
-private:
+protected:
     std::string name_;
     size_t amount_;
     size_t basePrice_;
