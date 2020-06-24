550c5eb20a6c66fe804dce9a7c63398f01132a6f
Adam Mierzwiak <5563693+adamvm@users.noreply.github.com>
2020-06-24 16:28:40 +0200

    (Task 7 + 8 part) finished

--- shm/Player.hpp
+++ shm/Player.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <memory>
+
+#include "Cargo.hpp"
 #include "Ship.hpp"
 
 class Player
@@ -12,7 +14,9 @@ public:
     };
 
     [[nodiscard]] size_t getMoney() const { return money_; }
-    [[nodiscard]] size_t getAvailableSpace() const { return availableSpace_; }
+    [[nodiscard]] size_t getAvailableSpace();
+    [[nodiscard]] size_t getSpeed() const { return ship_->getSpeed(); }
+    [[nodiscard]] Cargo* getCargo(size_t index) const { return ship_->getCargo(index); };
 
     void setMoney(size_t amount) { money_ = amount; }
     void setAvailableSpace(size_t space) { availableSpace_ = space; }
