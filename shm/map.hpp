f8ec116124f4c5fec81a4cf7c224cab31da35a37
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-06-24 17:02:43 +0200

    Adding the argument name in method signature

--- shm/map.hpp
+++ shm/map.hpp
@@ -8,7 +8,7 @@ class Map {
 public:
     Map();
     virtual ~Map();
-    Island *getIsland(const Coordinates&);
+    Island *getIsland(const Coordinates& coordinates);
     void printMap();
 
 private:
