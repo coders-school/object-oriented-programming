21da967a3fc325c25e61669cea1dd9ca984a81cb
mariomoore <mariusz.murawski84@gmail.com>
2020-06-24 22:50:15 +0200

    add currentPosition_ in class Map

--- shm/src/map.cpp
+++ shm/src/map.cpp
@@ -13,12 +13,14 @@ Map::Map() {
         auto posX = (size_t)distrib(gen);
         auto posY = (size_t)distrib(gen);
         if (std::none_of(islands_.begin(), islands_.end(), [posX, posY](auto& i) {
-                return i.get()->getPosition() == Coordinates(posX, posY);
+                return i->getPosition() == Coordinates(posX, posY);
             })) {
             islands_.push_back(std::make_shared<Island>(posX, posY));
             isls--;
         }
     } while (isls > 0);
+
+    currentPosition_ = islands_.front();
 }
 
 std::shared_ptr<Island> Map::getIsland(const Coordinates& coordinate) {
