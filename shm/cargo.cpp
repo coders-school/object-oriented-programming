1f7e4a83a77e31ae4d2b34c54c915a7a70725257
Marcinati <65040642+Marcinati@users.noreply.github.com>
2020-06-24 11:43:42 +0200

    make virtual

--- shm/cargo.cpp
+++ shm/cargo.cpp
@@ -2,31 +2,3 @@
 
 Cargo::Cargo(std::string name, size_t amount, size_t basePrice)
     : name_(name), amount_(amount), basePrice_(basePrice) {}
-
-Cargo& Cargo::operator+=(size_t amount) {
-    if (amount <= ship_->countFreeSpace()) {
-        amount_ += amount;
-    }
-    return *this;
-}
-Cargo& Cargo::operator-=(size_t amount) {
-    if (amount_ > amount) {
-        amount_ -= amount;
-    }
-    return *this;
-}
-
-bool Cargo::operator==(const Cargo& cargo) const {
-    return Cargo.getBasePrice() == basePrice_ && Cargo.getName() == name_;
-}
-
-std::string Cargo::getName() const {
-    return name_;
-}
-size_t Cargo::getAmount() const {
-    return amount_;
-}
-size_t Cargo::getBasePrice() const {
-    return basePrice_;
-}
-
