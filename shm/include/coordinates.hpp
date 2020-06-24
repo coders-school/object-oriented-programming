7534c06b1541f3890a11fc4b1a6023d110ccb673
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-06-24 21:04:41 +0200

    adding distance func to coordinates.hpp

--- shm/include/coordinates.hpp
+++ shm/include/coordinates.hpp
@@ -4,6 +4,7 @@ class Coordinates {
 public:
     Coordinates(int positionX, int positionY);
     bool operator==(const Coordinates& other);
+    static size_t distance(const Coordinates& lhs, const Coordinates& rhs);
 
 private:
     int positionX_;
--- shm/src/coordinates.cpp
+++ shm/src/coordinates.cpp
@@ -1,8 +1,16 @@
 #include "coordinates.hpp"
 
+#include <cmath>
+
 Coordinates::Coordinates(int positionX, int positionY)
     : positionX_(positionX), positionY_(positionY) {}
 
 bool Coordinates::operator==(const Coordinates& other) {
     return (positionX_ == other.positionX_ && positionY_ == other.positionY_);
+}
+
+size_t Coordinates::distance(const Coordinates& lhs, const Coordinates& rhs) {
+    int x_distance = lhs.positionX_ - rhs.positionX_;
+    int y_distance = lhs.positionY_ - rhs.positionY_;
+    return std::sqrt(x_distance * x_distance + y_distance * y_distance);
 }
