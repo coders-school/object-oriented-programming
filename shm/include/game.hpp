f361f86e185a171d6f9d3183d09c14f065b58470
mariomoore <mariusz.murawski84@gmail.com>
2020-06-24 22:51:33 +0200

    class Game updated

--- shm/include/game.hpp
+++ shm/include/game.hpp
@@ -1,18 +1,43 @@
 #pragma once
 
-#include <cstdlib>
+#include <memory>
+#include "map.hpp"
+#include "player.hpp"
+#include "time.hpp"
 
 class Game {
 public:
     Game();
-    Game(size_t start_money, size_t game_days, size_t final_goal)
-        : start_money_(start_money), game_days_(game_days), final_goal_(final_goal) {
-        Game();
-    };
+    Game(size_t start_money, size_t game_days, size_t final_goal);
     void startGame();
+    enum class Action {
+        Exit,
+        Travel,
+        Buy,
+        Sell,
+        PrintCargo,
+    };
 
 private:
     size_t start_money_ = 1'000;
     size_t game_days_ = 100;
     size_t final_goal_ = 2'000;
+    std::unique_ptr<Map> map_;
+    std::unique_ptr<Time> time_;
+    std::shared_ptr<Ship> ship_;
+    std::unique_ptr<Player> player_;
+
+    std::shared_ptr<Ship> generateShip();
+    bool checkWinCondition() const;
+    bool checkLooseCondition() const;
+    void printTopBar() const;
+    void printOptions() const;
+    void printWinScreen() const;
+    void printLooseScreen() const;
+    void makeAction(Action action);
+    void travel();
+    void buy();
+    void sell();
+    void printCargo();
+    void exit();
 };
--- shm/src/game.cpp
+++ shm/src/game.cpp
@@ -1,9 +1,105 @@
 #include "game.hpp"
 
+#include <iostream>
+
 Game::Game() {
+    map_ = std::make_unique<Map>();
+    time_ = std::make_unique<Time>(game_days_);
+    ship_ = generateShip();
+    player_ = std::make_unique<Player>(ship_, start_money_);
+}
+
+Game::Game(size_t start_money, size_t game_days, size_t final_goal)
+        : start_money_(start_money), game_days_(game_days), final_goal_(final_goal) {
+        Game();
+    };
+
+void Game::startGame() {
+    while(time_->getElapsedTime() > 0) {
+        if (checkWinCondition()) {
+            printWinScreen();
+            exit();
+        }
+        if (checkLooseCondition()) {
+            printLooseScreen();
+            exit();
+        }
+        printTopBar();
+        printOptions();
+        size_t action;
+        std::cin.clear();
+        std::cin >> action;
+        makeAction(static_cast<Action>(action));
+    }
+}
+
+std::shared_ptr<Ship> generateShip() {
+    return std::make_shared<Ship>(1, "Maria", 12, 128, 1200);
+}
+
+bool Game::checkWinCondition() const {
+    return player_->getMoeny() >= final_goal_;
+}
+
+bool Game::checkLooseCondition() const {
+    return player_->getMoeny() == 0 || time_->getElapsedTime() == 0;
+}
+
+void Game::printTopBar() const {
+    std::cout << "Top\n";
+}
+
+void Game::printOptions() const {
+    std::cout << "Options\n";
+}
+
+void Game::printWinScreen() const {
+    std::cout << "Win\n";
+}
+
+void  Game::printLooseScreen() const {
+    std::cout << "Loose\n";
+}
+
+void Game::makeAction(Action action) {
+    switch(action) {
+    case Action::Exit:
+        exit();
+        break;
+    case Action::Travel:
+        travel();
+        break;
+    case Action::Buy:
+        buy();
+        break; 
+    case Action::Sell:
+        sell();
+        break;
+    case Action::PrintCargo:
+        printCargo();
+        break;
+    default:
+        std::cout << "Wrong, Sailor!\n";
+        break;
+    }
+}
+
+void Game::travel() {
 
 }
 
-void Game::startGame() {
-    
+void Game::buy() {
+
+}
+
+void Game::sell() {
+
+}
+
+void  Game::printCargo() {
+
+}
+
+void  Game::exit() {
+
 }
