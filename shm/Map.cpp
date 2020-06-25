109b8643ea8919c0db100e30f0aa1f5398f0cec2
Adrian Baranowicz <adrbaranowicz@gmail.com>
2020-06-25 23:57:23 +0200

    Random islands generation on a newly constructed Map

--- shm/Map.cpp
+++ shm/Map.cpp
@@ -1,1 +1,31 @@
 #include "Map.hpp"
+#include <algorithm>
+#include <iostream>
+#include <random>
+#include <set>
+
+Map::Map() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> getRandomWidth(0, MAP_WIDTH);
+    std::uniform_int_distribution<> getRandomHeight(0, MAP_HEIGHT);
+    std::set<std::pair<size_t, size_t>> uniquePositions;
+    bool isUniquenessConditionMet = ((MAP_WIDTH + 1) * (MAP_HEIGHT + 1)) >= NUMBER_OF_ISLANDS;
+
+    if (isUniquenessConditionMet) {
+        while (uniquePositions.size() < NUMBER_OF_ISLANDS) {
+            size_t x = getRandomWidth(gen);
+            size_t y = getRandomHeight(gen);
+            uniquePositions.insert({x, y});
+        }
+    }
+    else {
+        std::cerr << "Could not generate " << NUMBER_OF_ISLANDS << " unique islands with provided map size!";
+    }
+
+    std::for_each(uniquePositions.begin(), 
+                  uniquePositions.end(), 
+                  [](const auto& position) { 
+                      islands_.push_back(Island(position.first, position.second));
+                  });
+}
--- shm/Map.hpp
+++ shm/Map.hpp
@@ -3,7 +3,14 @@
 #include "Island.hpp"
 #include <vector>
 
+constexpr auto NUMBER_OF_ISLANDS = 10;
+constexpr auto MAP_WIDTH = 100;
+constexpr auto MAP_HEIGHT = 100;
+
 class Map {
+public:
+    Map();
+
 private:
     std::vector<Island&> islands_;
     Island* currentPosition_;
