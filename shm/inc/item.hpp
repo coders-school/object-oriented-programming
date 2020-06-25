2bd03bcf8a7d42c60da2f707668c9ff0a7a2c03a
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-06-25 21:40:00 +0200

    Item class implemented - header

---
+++ shm/inc/item.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <string>
+
+#include "cargo.hpp"
+
+class Item : public Cargo {
+public:
+    enum class Rarity {
+        common = 1,
+        rare = 4,
+        epic = 8,
+        legendary = 16
+    };
+
+    Item(const std::string name, size_t amount, size_t basePrice, Rarity rarity)
+            : Cargo(name, amount, basePrice)
+            , rairty_(rarity)
+    {}
+    
+    Item& operator--();
+    Item& operator--(int);
+    bool operator==(const Cargo& item) const;
+    bool operator!=(const Cargo& item) const;
+    Cargo& operator+=(size_t amount);
+    Cargo& operator-=(size_t amount);
+
+    size_t getPrice() const override;
+    std::string getName() const override;
+    size_t getAmount() const override;
+    size_t getBasePrice() const override;
+    Rarity getRarity() const;
+
+private:
+    const Rarity rairty_;
+};
