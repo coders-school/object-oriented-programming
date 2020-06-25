89e84f606aa780f12a9cb9a7ffde202b2971eaba
ArturGubala <gubala-artur@wp.pl>
2020-06-25 12:22:40 +0200

    add functions which return particular cargo and all cargos, refactor Player function countAvailableSpace

--- shm/Ship.hpp
+++ shm/Ship.hpp
@@ -9,27 +9,21 @@
 class Ship {
 public:
     Ship()
-        : id_(0)
-    {}
+        : id_(0) {}
     Ship(int capacity, int maxCrew, int speed, const std::string& name, size_t id)
-        : capacity_(capacity)
-        , maxCrew_(maxCrew)
-        , crew_(0)
-        , speed_(speed)
-        , name_(name)
-        , id_(id)
-    {}
+        : capacity_(capacity), maxCrew_(maxCrew), crew_(0), speed_(speed), name_(name), id_(id) {}
     Ship(int maxCrew, int speed, size_t id)
-        : Ship(0, maxCrew, speed, "", id)
-    {}
+        : Ship(0, maxCrew, speed, "", id) {}
 
     void setName(const std::string& name) { name_ = name; }
 
-    size_t getCapacity() const  { return capacity_; }
-    size_t getMaxCrew() const   { return maxCrew_; }
-    size_t getSpeed() const     { return speed_; }
+    size_t getCapacity() const { return capacity_; }
+    size_t getMaxCrew() const { return maxCrew_; }
+    size_t getSpeed() const { return speed_; }
     std::string getName() const { return name_; }
-    size_t getId() const        { return id_; }
+    size_t getId() const { return id_; }
+    Cargo* getCargo(size_t index) const { return cargo_[index].get(); };
+    std::vector<std::shared_ptr<Cargo>> getAllCargos() const { return cargo_; }
 
     Ship& operator-=(const size_t crew);
     Ship& operator+=(const size_t crew);
