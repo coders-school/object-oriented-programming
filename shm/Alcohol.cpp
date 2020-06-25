8ff1e73c319e6eb03df411106abcc7b38fa7a0b3
Nessy90 <nessy0991@gmail.com>
2020-06-25 20:59:37 +0200

    add Alcohol class

---
+++ shm/Alcohol.cpp
@@ -0,0 +1,23 @@
+#include "Alcohol.hpp"
+
+size_t Alcohol::getPrice() const {
+    return _basePrice * (_power / MAX_POWER);
+}
+
+Cargo& Alcohol::operator+=(const size_t amount) {
+    if (_amount + amount > _maxAmount) {
+        std::cerr << "Not enought space for that cargo!\n";
+        return *this;
+    }
+    _amount += amount;
+    return *this;
+}
+
+Cargo& Alcohol::operator-=(const size_t amount) {
+    if (_amount < amount) {
+        std::cerr << "You don't have enough cargo!\n";
+        return *this;
+    }
+    _amount -= amount;
+    return *this;
+}
