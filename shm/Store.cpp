e682d9663c98528d737647eff9c83eb87f1258e9
Tomonike <pgurtowski1@gmail.com>
2020-06-26 15:01:37 +0200

    Add lack of space Store::Buy

--- shm/Store.cpp
+++ shm/Store.cpp
@@ -6,6 +6,9 @@
 Store::Response Store::Buy(Cargo* cargo, size_t amount, Player* player) {
     if (player->getAvailableSpace() < amount)
         return Response::lack_of_space;
+    if (player->getAvailableSpace() < amount)
+        return Response::lack_of_space;
+
     return Response::done;
 };
 
