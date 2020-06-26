f38c466fe54df4c6ef82a888cbd87be31a00bbf0
Tomonike <pgurtowski1@gmail.com>
2020-06-26 09:43:29 +0200

    Store enum class Response

---
+++ shm/Store.cpp
@@ -0,0 +1,3 @@
+#include "Store.hpp"
+
+#include <string>
--- shm/Store.hpp
+++ shm/Store.hpp
@@ -1,8 +1,15 @@
 #pragma onec
+#include <string>
 
-enum class Response {
-    done,
-    lack_of_money,
-    lack_of_cargo,
-    lack_of_space
+class Store {
+public:
+    enum class Response {
+        done,
+        lack_of_money,
+        lack_of_cargo,
+        lack_of_space
+    };
+
+
+private:
 };
