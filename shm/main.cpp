357f0ba77d928d17f57cd6ff2c63a06030790c35
Marcinati <marcin.kapusta.pk@gmail.com>
2020-06-27 21:59:38 +0200

    for compile and main for testing

--- shm/main.cpp
+++ shm/main.cpp
@@ -1,31 +1,34 @@
-#include "alcohol.hpp"
 #include "cargo.hpp"
 #include "coordinates.hpp"
-#include "fruit.hpp"
 #include "island.hpp"
-#include "item.hpp"
 #include "map.hpp"
 //#include "observer.hpp" not ready yet
 #include "player.hpp"
 #include "ship.hpp"
-//#include "time.hpp" not ready yet
+#include "time.hpp"
 
 #include <iostream>
 #include <memory>
+#include <string>
+
+void printTrail() {
+    const std::string floor(50, '-');
+    std::cout << floor << '\n';
+}
 
 int main() {
-    //Ship T(20, 100, -5);
-    //std::unique_ptr<Ship> uPtrT = std::make_unique<Ship>(T);
-    //Ship FD(100, 50, 12, "Flying Dutchman", 1);
-    //std::unique_ptr<Ship> uPtrFD = std::make_unique<Ship>(FD);
-    //int capacity, int maxCrew, int speed, const std::string& name, int id
-    //std::cout << FD.getCapacity();  
-    // size_t getMaxCrew() const; 
-    // size_t getSpeed() const;
-    // std::string getName() const;
-    // int getId() const;
-    // Cargo* getCargo(size_t cargo);
-    // std::vector<Cargo> getAllCargo() const;
+    printTrail();
+    std::cout << "Test of SHM in main.cpp\n";
+    Ship FD(100, 50, 12, "Flying Dutchman", 1);
+    printTrail();
+    std::cout << "Ship FD has been created: \n";
+    printTrail();
+    std::cout << "Capacity: " << FD.getCapacity() << '\n';
+    std::cout << "Max crew: " << FD.getMaxCrew() << '\n';
+    std::cout << "Speed: " << FD.getSpeed() << '\n';
+    std::cout << "Name: " << FD.getName() << '\n';
+    std::cout << "Id: " << FD.getId() << '\n';
 
+    //std::unique_ptr<Ship> uPtrFD = std::make_unique<Ship>(FD);
     return 0;
 }
