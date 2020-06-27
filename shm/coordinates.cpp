297f89bc16a14d77a647992bdafac4c936366667
Kasperice <kasper@alert.pl>
2020-06-27 09:55:29 +0200

    Task 2.7 and 2.8 start

--- shm/coordinates.cpp
+++ shm/coordinates.cpp
@@ -1,8 +1,8 @@
 #include "coordinates.hpp"
 
-Coordinates::Coordinates() : positionX_(0), positionY_(0){};
+Coordinates::Coordinates() : positionX_(0), positionY_(0) {}
 
-Coordinates::Coordinates(int32_t positionX, int32_t positionY) : positionX_(positionX), positionY_(positionY){};
+Coordinates::Coordinates(int32_t positionX, int32_t positionY) : positionX_(positionX), positionY_(positionY) {}
 
 bool Coordinates::operator==(const Coordinates& position) {
     return positionX_ == position.positionX_ && positionY_ == position.positionY_;
