38bc266212b6cd7798ad7d8ac3651fa58de76d7f
Pawellos <pablox377@gmail.com>
2020-06-27 14:17:32 +0200

    Add makefile

--- shm/player.cpp
+++ shm/player.cpp
@@ -8,11 +8,9 @@ Player::Player(std::shared_ptr<Ship> ship, uint32_t money, uint32_t availableSpa
 std::shared_ptr<Ship> Player::getShip() {
     return ship_;
 }
-
 uint32_t Player::getAvailableSpace() const {
     return availableSpace_;
 }
-
 uint32_t Player::getMoney() const {
     return money_;
 }
@@ -28,8 +26,7 @@ uint32_t Player::CountAvailableSpace() const {
 uint32_t Player::getSpeed() const {
     return ship_->getSpeed();
 }
-
-Cargo* Player::getCargo(uint32_t index) const {
+std::shared_ptr<Cargo> Player::getCargo(uint32_t index) const {
     if (ship_)
         return ship_->getCargo(index);
     return nullptr;
@@ -38,4 +35,14 @@ Cargo* Player::getCargo(uint32_t index) const {
 void Player::purchaseCargo(std::shared_ptr<Cargo> cargo, uint32_t amount, uint32_t price) {
     availableSpace_ -= amount;
     money_ -= price;
+    ship_->load(cargo);
+}
+
+void Player::sellCargo(std::shared_ptr<Cargo> cargo, uint32_t amount, uint32_t price) {
+    availableSpace_ += amount;
+    money_ += price;
+    ship_->unload(cargo, amount);
+}
+void Player::printCargo() const {
+    ship_->printCargo();
 }
--- shm/ship.cpp
+++ shm/ship.cpp
@@ -1,21 +1,18 @@
 #include "ship.hpp"
 
+#include <algorithm>
 #include <iostream>
 #include <string>
 
 Ship::Ship() : id_(-1){};
 
 Ship::Ship(uint32_t capacity, uint32_t maxCrew, uint32_t speed, const std::string& name, uint32_t id)
-    : capacity_(capacity), maxCrew_(maxCrew), crew_(0), speed_(speed), name_(name), id_(id) {}
+    : capacity_(capacity), maxCrew_(maxCrew), crew_(0), speed_(speed), name_(name), id_(id){};
 
-Ship::Ship(uint32_t maxCrew, uint32_t speed, uint32_t id) : Ship(0, maxCrew, speed, "", id) {}
+Ship::Ship(uint32_t maxCrew, uint32_t speed, uint32_t id) : Ship(0, maxCrew, speed, "", id){};
 
 void Ship::setName(const std::string& name) {
     name_ = name;
-}
-
-void Ship::addToCargo(Cargo cargo) {
-    cargo_.push_back(cargo);
 }
 
 Ship& Ship::operator-=(uint32_t crew) {
@@ -60,10 +57,43 @@ uint32_t Ship::getCapacity() const {
     return capacity_;
 }
 
-Cargo* Ship::getCargo(size_t index) {
-    return &cargo_[index];
+std::shared_ptr<Cargo> Ship::getCargo(size_t index) const {
+    if (cargo_.size() > index) {
+        return cargo_[index];
+    }
+    return nullptr;
 }
 
-std::vector<Cargo> Ship::getVectorCargo() const {
+std::vector<std::shared_ptr<Cargo>> Ship::getVectorCargo() const {
     return cargo_;
+}
+
+void Ship::load(std::shared_ptr<Cargo> cargo) {
+    for (auto& element : getVectorCargo()) {
+        if (cargo.get()->getName() == element->getName()) {
+            *element += cargo.get()->getAmount();
+            return;
+        }
+    }
+    cargo_.emplace_back(cargo);
+}
+
+void Ship::unload(std::shared_ptr<Cargo> cargo, uint32_t amount) {
+    auto it = std::find_if(cargo_.begin(), cargo_.end(),
+                           [cargo](const auto& ptr) { return ptr->getName() == cargo->getName(); });
+    if (it != cargo_.end()) {
+        if (amount == it->get()->getAmount()) {
+            cargo_.erase(it);
+        } else {
+            *cargo -= amount;
+        }
+    };
+}
+
+void Ship::printCargo() const {
+    std::cout << "CARGO ON SHIP : "
+              << "\n";
+    std::for_each(cargo_.begin(), cargo_.end(), [](const auto& cargo) {
+        std::cout << cargo->getName() << "  " << cargo->getAmount() << "  " << cargo->getBasePrice() << "\n";
+    });
 }
