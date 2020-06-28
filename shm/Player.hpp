75de34b1cbf2bcd4cbbc337104c0b53e4a9eb051
Adam Mierzwiak <5563693+adamvm@users.noreply.github.com>
2020-06-28 09:23:54 +0200

    Task 8

--- shm/Player.hpp
+++ shm/Player.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include "Cargo.hpp"
 #include "Ship.hpp"
 
 class Player
@@ -11,16 +12,17 @@ public:
         ship_ = std::make_unique<Ship>(std::move(ship));
     };
 
-    [[nodiscard]] size_t getMoney() const { return money_; }
-    [[nodiscard]] size_t getAvailableSpace() const { return availableSpace_; }
-    [[nodiscard]] size_t getSpeed() const { return ship_->getSpeed(); }
-    [[nodiscard]] Cargo* getCargo(size_t index) const { return ship_->getCargo(index); };
+    [[nodiscard]] size_t getAvailableSpace() const;
+    [[nodiscard]] size_t getMoney() const;
+    [[nodiscard]] size_t getSpeed() const;
+    [[nodiscard]] Cargo* getCargo(size_t index) const;
 
-    void setMoney(size_t amount) { money_ = amount; }
-    void setAvailableSpace(size_t space) { availableSpace_ = space; }
-
+    void setMoney(size_t amount);
 private:
+
     std::unique_ptr<Ship> ship_;
     size_t money_;
     size_t availableSpace_;
+
+    void updateAvailableSpace();
 };
