60336c3c254271294de42cf76ecbe450f17e74eb
marekjanek <marekjanus3@wp.pl>
2020-06-28 18:15:57 +0200

    + nextday()

--- shm/include/store.hpp
+++ shm/include/store.hpp
@@ -2,6 +2,7 @@
 
 #include "cargo.hpp"
 #include "player.hpp"
+#include "time.hpp"
 
 enum class Response {
     done,
@@ -14,5 +15,8 @@ class Store {
     Store();
     Response buy(Cargo* cargo, size_t amount, Player* player);
     Response sell(Cargo* cargo, size_t amount, Player* player);
+    void nextDay() override;
+    std::vector<cargoPtr> cargo_;
+
     // dopisać składowe - towar jaki sklep przetrzymuje i obsługa nextday ;)
 };
--- shm/src/store.cpp
+++ shm/src/store.cpp
@@ -25,8 +25,16 @@ Response Store::sell(Cargo* cargo, size_t amount, Player* player) {
     if (player->getShip()->FindMatchCargo(cargo)->getAmount() < amount)
         return Response::lack_of_cargo;
 
+    cargo += amount;
+
     size_t money = amount * cargo->getPrice();
     player->addMoney(money);
 
     return Response::done;
+}
+
+void Store::nextDay() {
+    for (auto el : cargo_) {
+        el += 1;
+    }
 }
