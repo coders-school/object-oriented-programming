0a95e0661b66410333ac4b4c156228af398c84ba
kozlofski <kozlofski.dev@gmail.com>
2020-06-29 09:57:17 +0200

    Task 1 from during online lecture
    Cargo is now an interface
    getters made virtual

--- shm/cargo.hpp
+++ shm/cargo.hpp
@@ -10,9 +10,10 @@ public:
     Cargo& operator-=(size_t amount);
     bool operator==(const Cargo& rhs) const;
 
-    std::string getName() const { return name_; }
-    size_t getAmount() const { return amount_; }
-    size_t getBasePrice() const { return basePrice_; }
+    virtual std::string getName() const = 0;
+    virtual size_t getAmount() const = 0;
+    virtual size_t getBasePrice() const = 0;
+    virtual size_t getPrice() const = 0;
 
 private:
     std::string name_{};
