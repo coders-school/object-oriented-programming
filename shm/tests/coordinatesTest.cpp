21812dec098ea6b358e4af38fc29ecb2f48071e8
Andrzej Rafalski <anraf1001@gmail.com>
2020-06-29 20:00:33 +0200

    coordinatesTest

--- shm/tests/coordinatesTest.cpp
+++ shm/tests/coordinatesTest.cpp
@@ -1,6 +1,26 @@
+#include "coordinates.hpp"
 #include "gtest/gtest.h"
 
-#include "coordinates.hpp"
+TEST(CoordinatesTest, shouldCheckIfCoordsAreEqual) {
+    Coordinates lhs(2, 4);
+    Coordinates rhs(2, 4);
+    EXPECT_EQ(lhs, rhs);
+}
 
-// Define your first test here...
+TEST(CoordinatesTest, shouldCountDistanceBetweenCoordsXAxis) {
+    Coordinates lhs(2, 4);
+    Coordinates rhs(5, 4);
+    EXPECT_EQ(Coordinates::distance(lhs, rhs), 3);
+}
 
+TEST(CoordinatesTest, shouldCountDistanceBetweenCoordsYAxis) {
+    Coordinates lhs(2, 4);
+    Coordinates rhs(2, 2);
+    EXPECT_EQ(Coordinates::distance(lhs, rhs), 2);
+}
+
+TEST(CoordinatesTest, shouldCountDistanceBetweenCoords) {
+    Coordinates lhs(1, 4);
+    Coordinates rhs(-2, 9);
+    EXPECT_EQ(Coordinates::distance(lhs, rhs), 5);
+}
