c9ca7a84b6ead721a3fe462ec867be333b2713ee
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-06-29 10:44:26 +0200

    checking if observers_ is not empty before removing

--- shm/src/time.cpp
+++ shm/src/time.cpp
@@ -10,6 +10,9 @@ void Time::addObserver(Observer* observer) {
     observers_.push_back(observer);
 }
 void Time::removeObserver(Observer* observer) {
+    if (observers_.size() <= 0) {
+        return;
+    }
     observers_.erase(std::remove(observers_.begin(), observers_.end(), observer));
 }
 void Time::notifyObservers() {
