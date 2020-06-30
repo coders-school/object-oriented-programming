da64037fbfa2b6ad130e1fc5c4f791ac3447f370
majkel <michal.sobotka.84@gmail.com>
2020-06-30 22:52:38 +0200

    Class Player hotfix

--- shm/include/player.hpp
+++ shm/include/player.hpp
@@ -9,22 +9,17 @@ public:
     virtual ~Player() = default;
 
     std::shared_ptr<Ship> getShip() const { return ship_; }
-    size_t getMoeny() const { return money_; }
+    size_t getMoney() const { return money_; }
     size_t getSpeed() const { return ship_->getSpeed(); }
-    
     cargoPtr getCargo(size_t index) const { return ship_->getCargo()[index]; }
-
+    size_t getAvailableSpace() const;
     void setShip(const std::shared_ptr<Ship>& ship);
     void addMoney(const size_t& money);
-
     void payCrew(size_t money) override;
     void subtractMoney(const size_t& money);
-
 
 private:
     std::shared_ptr<Ship> ship_;
     size_t money_;
     size_t availableSpace_;
-
-    size_t getAvailableSpace() const;
 };
