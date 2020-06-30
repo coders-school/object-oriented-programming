b049ba7054d9d26085cf60336da7978222f5b7ba
Miroslaw Kopertowski <miroslaw.kopertowski@tieto.com>
2020-06-30 13:42:08 +0200

    add class Time and Observer interface

---
+++ shm/inc/time.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "observer.hpp"
+#include <vector>
+
+class Time {
+public:
+    Time();
+
+    void AddObserver(Observer *obs);
+    void RemoveObserver(Observer *obs);
+    size_t GetElapsedTime() const {return time_elapsed_; };
+    Time& operator++();
+
+private:
+    size_t time_elapsed_{};
+    std::vector<Observer*> observers_;
+    
+    void Notify();
+};
