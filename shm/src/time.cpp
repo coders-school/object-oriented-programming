5136aea46c8c1d34c86d218ab432f78cb38eeebc
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-07-01 10:19:03 +0200

    changing removeObserver function in time.cpp

--- shm/src/time.cpp
+++ shm/src/time.cpp
@@ -10,10 +10,7 @@ void Time::addObserver(Observer* observer) {
     observers_.push_back(observer);
 }
 void Time::removeObserver(Observer* observer) {
-    if (observers_.size() <= 0) {
-        return;
-    }
-    observers_.erase(std::remove(observers_.begin(), observers_.end(), observer));
+    observers_.erase(std::remove(observers_.begin(), observers_.end(), observer), observers_.end());
 }
 void Time::notifyObservers() {
     for (auto observer : observers_)
