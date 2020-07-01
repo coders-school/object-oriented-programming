870a2a5cc80998afa3d7eae6f7ef36ed85687fd3
Tomasz Eichler <tomasz.eichler@wp.pl>
2020-07-01 14:27:13 +0200

    Implemented fruit class with override methods and new expiryDate and timeElapsed fields

--- shm/fruit.cpp
+++ shm/fruit.cpp
@@ -1,29 +1,40 @@
-#include "cargo.hpp"
 #include "fruit.hpp"
+
 #include <string>
 
-    //Override from Cargo
-Fruit::Fruit(std::string name, size_t amount, size_t basePrice):
-    Cargo(name, amount, basePrice){}
+//Override from Cargo
+Fruit::Fruit(std::string name, size_t amount, size_t basePrice, size_t expiryDate, size_t timeElapsed)
+    : Cargo(name, amount, basePrice),
+      expiryDate_(expiryDate),
+      timeElapsed_(timeElapsed) {}
 
-    size_t Fruit::getPrice() const {
-        return basePrice_ * (timeToSpoil_ / expiryDate_);
+size_t Fruit::getPrice() const {
+    if (timeToSpoil_) {
+        return static_cast<size_t>(basePrice_ * static_cast<float>(timeToSpoil_) / expiryDate_);
     }
-    std::string Fruit::getName() const {
-        return name_;
-    }
-    size_t Fruit::getAmount() const {
-        return amount_;
-    }
-    size_t Fruit::getBasePrice() const {
-        return basePrice_;
-    }
-    
-    Fruit& Fruit::operator--() {
-        if (expiryDate_ == 0) {
-            return *this;
-        }
-        --timeToSpoil_;
-        return *this;
-    }
+    return 0;
+}
+std::string Fruit::getName() const {
+    return name_;
+}
+size_t Fruit::getAmount() const {
+    return amount_;
+}
+size_t Fruit::getBasePrice() const {
+    return basePrice_;
+}
 
+Fruit& Fruit::operator--() {
+    if (amount_) {
+        --amount_;
+    }
+    return *this;
+}
+
+size_t Fruit::getExpiryDate() const {
+    return expiryDate_;
+}
+
+size_t Fruit::getTimeElapsed() const {
+    return timeElapsed_;
+}
