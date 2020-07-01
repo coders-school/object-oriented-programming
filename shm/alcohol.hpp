4e54cdaae4056760d21457c149d9de94584dfcc9
Tomasz Eichler <tomasz.eichler@wp.pl>
2020-07-01 14:35:06 +0200

    added virtual destructor

--- shm/alcohol.hpp
+++ shm/alcohol.hpp
@@ -8,6 +8,7 @@ constexpr size_t maxPower{96};
 class Alcohol : public Cargo {
 public:
     Alcohol(std::string name, size_t amount, size_t basePrice, size_t power);
+    ~Alcohol() override = default;
     //Override from Cargo
     size_t getPrice() const override;
     std::string getName() const override;
