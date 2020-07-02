6b6a0d24e41d8f15ff16f926556d5804b40743d6
Marcinati <65040642+Marcinati@users.noreply.github.com>
2020-07-02 10:32:49 +0200

    one class in one cpp

--- shm/testShip.cpp
+++ shm/testShip.cpp
@@ -1,6 +1,4 @@
 #include "gtest/gtest.h"
-#include "cargo.hpp"
-#include "item.hpp"
 #include "ship.hpp"
 
 #include <string>
@@ -17,18 +15,6 @@ protected:
     Ship shipOne;
 };
 
-class ItemTest : public ::testing::Test {
-public:
-    Item::Rarity rarity = Item::Rarity::rare;
-
-protected:
-    std::string name = "Old_Shoe";
-    size_t amount = 2;
-    size_t basePrice = 1000;
-
-    ItemTest() : itemOne(name, amount, basePrice, rarity) {}
-    Item itemOne;
-};
 
 TEST_F(ShipTest, ShouldChangeShipName) {
     shipOne.setName("Roaar");
@@ -71,9 +57,4 @@ TEST_F(ShipTest, ShouldNotAddNewMembersToShipCrewBecauseOfTooManyMembers) {
 
 TEST_F(ShipTest, ShouldCountAvailableCargoSpaceWhenShipHasNoCargoToSell) {
     ASSERT_EQ(shipOne.countAvailableSpace(), shipOne.getCapacity());
-}
-
-TEST_F(ItemTest, ShouldGivePriceOfItem) {
-    auto priceAfterRarity = itemOne.getBasePrice() * static_cast<int>(rarity);
-    ASSERT_EQ(itemOne.getPrice(), priceAfterRarity);
 }
