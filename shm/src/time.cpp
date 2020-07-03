9d540c87feeb828cca33cca2f7773b4b44251e6c
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-07-04 01:10:40 +0200

    Formatting improvements

--- shm/src/time.cpp
+++ shm/src/time.cpp
@@ -3,11 +3,13 @@
 #include <algorithm>
 #include <iostream>
 
-void Time::addObserver(Observer *obs) {
+void Time::addObserver(Observer* obs) {
     observers_.push_back(obs);
 }
-void Time::removeObserver(Observer *obs) {
-    observers_.erase(std::remove(observers_.begin(), observers_.end(), obs), observers_.end());
+void Time::removeObserver(Observer* obs) {
+    observers_.erase(std::remove(observers_.begin(),
+                                 observers_.end(), obs),
+                     observers_.end());
 }
 Time& Time::operator++() {
     ++time_elapsed_;
