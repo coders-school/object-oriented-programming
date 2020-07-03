993399aaa5798dd635ca929656a0d4a83e9ec4bd
lisie-sprawy <magdajankowska6@tlen.pl>
2020-07-03 18:16:25 +0200

    fixed wrong buy/sell amount

--- shm/Store.cpp
+++ shm/Store.cpp
@@ -12,14 +12,14 @@ Response Store::buy(std::shared_ptr<Cargo> cargo, size_t amount, Player* player)
     else if (player -> getAvaliableSpace() < amount){
         return Response::lack_of_space;
     }
-    *cargo -= amount;
+    *cargo += amount;
     player->buyCargo(cargo, amount, price);
     return Response::done;
 }
 
 Response Store::sell(std::shared_ptr<Cargo> cargo, size_t amount, Player* player) {
     const size_t price = amount * cargo->getPrice();
-    *cargo += amount;
+    *cargo -= amount;
     player->sellCargo(cargo, amount, price);
     return Response::done;
 }
