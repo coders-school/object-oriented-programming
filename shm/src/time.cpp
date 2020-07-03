df95844a74902404d4b15445d5ccffe8846da9a7
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-07-04 00:44:51 +0200

    Reference added in notify() method loop

--- shm/src/time.cpp
+++ shm/src/time.cpp
@@ -10,14 +10,14 @@ void Time::removeObserver(Observer *obs) {
     observers_.erase(std::remove(observers_.begin(), observers_.end(), obs), observers_.end());
 }
 Time& Time::operator++() {
-    time_elapsed_++;
+    ++time_elapsed_;
     notify();
     return *this;
 }
 
 void Time::notify() {
     std::cout << "<><><><>ObserversCount: " << observers_.size() << "\n";
-    for (const auto obs : observers_) {
+    for (const auto& obs : observers_) {
         obs->nextDay();
     }
 }
