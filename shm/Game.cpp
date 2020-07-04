9c7b65ae499f3cefe46a21024f4fdb66282223e7
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-07-04 15:30:01 +0200

    basic Game class

--- shm/Game.cpp
+++ shm/Game.cpp
@@ -1,2 +1,4 @@
 #include "Game.hpp"
 
+Game::Game(size_t money, size_t days, size_t final_goal)
+    : money_(money), days_(days), final_goal_(final_goal) {}
--- shm/Game.hpp
+++ shm/Game.hpp
@@ -1,2 +1,17 @@
 #pragma once
 
+#include "Map.hpp"
+#include "Player.hpp"
+
+class Game {
+private:
+    size_t money_;
+    size_t days_;
+    size_t final_goal_;
+    size_t current_day_;
+    Map* map_;
+    Player* player_;
+
+public:
+    Game(size_t money, size_t days, size_t final_goal);
+};
