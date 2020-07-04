b3bf49dfa505adf667f5dcef4ec7445a45d1f834
Marime <urziemska@gmail.com>
2020-07-04 20:00:40 +0200

    wip - task 3.1

--- shm/inc/fruit.hpp
+++ shm/inc/fruit.hpp
@@ -6,26 +6,27 @@
 
 class Fruit : public Cargo {
 public:
-    Fruit(const std::string& name, size_t amount, size_t basePrice, size_t timeToSpoil)
-            : Cargo(name, amount, basePrice)
-            , timeToSpoil_(timeToSpoil)
-    {}
-    
-    Fruit& operator--();
-    Fruit& operator--(int);
-    bool operator==(const Cargo& fruit) const;
-    bool operator!=(const Cargo& fruit) const;
-    Cargo& operator+=(size_t amount);
-    Cargo& operator-=(size_t amount);
+  Fruit(const std::string &name, size_t amount, size_t basePrice,
+        size_t timeToSpoil)
+      : Cargo(name, amount, basePrice), timeToSpoil_(timeToSpoil) {}
 
-    size_t getPrice() const override;
-    std::string getName() const override;
-    size_t getAmount() const override;
-    size_t getBasePrice() const override;
-    size_t getTimeToSpoil() const;
-    size_t getTimeToSpoilLeft() const;
+  Fruit &operator--();
+  Fruit &operator--(int);
+  bool operator==(const Cargo &fruit) const;
+  bool operator!=(const Cargo &fruit) const;
+  Cargo &operator+=(size_t amount);
+  Cargo &operator-=(size_t amount);
+
+  size_t getPrice() const override;
+  std::string getName() const override;
+  size_t getAmount() const override;
+  size_t getBasePrice() const override;
+  size_t getTimeToSpoil() const;
+  size_t getTimeToSpoilLeft() const;
+
+  void nextDay() override;
 
 private:
-    const size_t timeToSpoil_;
-    size_t timeToSpoilLeft_ = timeToSpoil_;
+  const size_t timeToSpoil_ = 5;
+  size_t timeToSpoilLeft_ = timeToSpoil_;
 };
--- shm/src/fruit.cpp
+++ shm/src/fruit.cpp
@@ -1,9 +1,9 @@
 #include "fruit.hpp"
 
 size_t Fruit::getPrice() const {
-    return static_cast<size_t>((static_cast<float>(timeToSpoilLeft_) /
-                                static_cast<float>(timeToSpoil_)) * 
-                                static_cast<float>(basePrice_));
+  return static_cast<size_t>((static_cast<float>(timeToSpoilLeft_) /
+                              static_cast<float>(timeToSpoil_)) *
+                             static_cast<float>(basePrice_));
 }
 
 std::string Fruit::getName() const { return name_; }
@@ -12,45 +12,46 @@ size_t Fruit::getBasePrice() const { return basePrice_; }
 size_t Fruit::getTimeToSpoil() const { return timeToSpoil_; }
 size_t Fruit::getTimeToSpoilLeft() const { return timeToSpoilLeft_; }
 
-Fruit& Fruit::operator--() {
-    if (timeToSpoilLeft_ > 0) {
-        --timeToSpoilLeft_;
-    }
+Fruit &Fruit::operator--() {
+  if (timeToSpoilLeft_ > 0) {
+    --timeToSpoilLeft_;
+  }
+  return *this;
+}
+Fruit &Fruit::operator--(int) {
+  Fruit &temp(*this);
+  if (timeToSpoilLeft_ > 0) {
+    operator--();
+  }
+  return temp;
+}
+bool Fruit::operator==(const Cargo &cargo) const {
+  if (typeid(cargo) == typeid(Fruit)) {
+    const Fruit *fruit = static_cast<const Fruit *>(&cargo);
+    return name_ == fruit->getName() && amount_ == fruit->getAmount() &&
+           basePrice_ == fruit->getBasePrice() &&
+           timeToSpoil_ == fruit->getTimeToSpoil() &&
+           timeToSpoilLeft_ == fruit->getTimeToSpoilLeft();
+  }
+  return false;
+}
+bool Fruit::operator!=(const Cargo &cargo) const {
+  if (typeid(cargo) == typeid(Fruit)) {
+    const Fruit *fruit = static_cast<const Fruit *>(&cargo);
+    return !(this == fruit);
+  }
+  return false;
+}
+Cargo &Fruit::operator+=(size_t amount) {
+  amount_ += amount;
+  return *this;
+}
+Cargo &Fruit::operator-=(size_t amount) {
+  if (amount <= amount_) {
+    amount_ -= amount;
     return *this;
+  }
+  return *this;
 }
-Fruit& Fruit::operator--(int) {
-    Fruit& temp(*this);
-    if (timeToSpoilLeft_ > 0) {
-        operator--();
-    }
-    return temp;
-}
-bool Fruit::operator==(const Cargo& cargo) const {
-    if (typeid(cargo) == typeid(Fruit)) {
-        const Fruit* fruit = static_cast<const Fruit*>(&cargo);
-        return name_ == fruit->getName() &&
-            amount_ == fruit->getAmount() &&
-            basePrice_ == fruit->getBasePrice() &&
-            timeToSpoil_ == fruit->getTimeToSpoil() &&
-            timeToSpoilLeft_ == fruit->getTimeToSpoilLeft();
-    }
-    return false;
-}
-bool Fruit::operator!=(const Cargo& cargo) const {
-    if (typeid(cargo) == typeid(Fruit)) {
-        const Fruit* fruit = static_cast<const Fruit*>(&cargo);
-        return !(this == fruit);
-    }
-    return false;
-}
-Cargo& Fruit::operator+=(size_t amount) {
-    amount_ += amount;
-    return *this;
-}
-Cargo& Fruit::operator-=(size_t amount) {
-    if (amount <= amount_) {
-        amount_ -= amount;
-        return *this;
-    }
-    return *this;
-}
+
+void Fruit::nextDay() { --timeToSpoilLeft_; }
