c16dd6c82f64d0093bd2915cee9d01e235dbb2c8
Jakub Cieplinski <cieplinski.jakubsp@gmail.com>
2020-07-04 19:18:22 +0200

    Time implemented, As of now future Game class should use it to increment, check and notify. It may change

--- shm/Time.cpp
+++ shm/Time.cpp
@@ -6,7 +6,27 @@ void Time::addObserver(std::shared_ptr<Observer> &observer) {
 }
 
 void Time::removeObserver(std::shared_ptr<Observer> &observer) {
+    observers_.erase(std::remove(observers_.begin(), observers_.end(), observer));
+}
 
+Time &Time::operator++() {
+    elapsedTime_++;
+    return *this;
+}
 
+void Time::notifyObservers() {
+    std::for_each(observers_.begin(), observers_.end(), [](const auto& el){el->nextDay();});
+}
 
+size_t Time::getElapsedTime() const {
+    return elapsedTime_;
+}
+
+void Time::resetTime() {
+    elapsedTime_ = 0;
+}
+
+void Time::sendNotification() {
+    notifyObservers();
+    resetTime();
 }
--- shm/Time.hpp
+++ shm/Time.hpp
@@ -6,11 +6,15 @@
 
 class Time {
 private:
-    std::vector<std::shared_ptr<Observer>> observers_;
-    size_t elapsedTime_;
+    std::vector<std::shared_ptr<Observer>> observers_ {};
+    size_t elapsedTime_ {0};
+
+    void resetTime();
+    void notifyObservers();
 public:
-    Time();
     void addObserver(std::shared_ptr<Observer> &observer);
     void removeObserver(std::shared_ptr<Observer> &observer);
-
+    void sendNotification();
+    size_t getElapsedTime() const;
+    Time& operator++();
 };
