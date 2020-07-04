a98bd30521776e3158c47911698441a0107ad70b
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-07-04 15:04:36 +0200

    fixed files

--- shm/Game.cpp
+++ shm/Game.cpp
@@ -1,1 +1,10 @@
 #include "Game.hpp"
+
+constexpr size_t start_money = 1000;
+constexpr size_t game_days = 100;
+constexpr size_t final_goal = 2000;
+
+int main() {
+
+    return 0;
+}
--- shm/Game.hpp
+++ shm/Game.hpp
@@ -1,10 +1,2 @@
 #pragma once
 
-constexpr size_t start_money = 1000;
-constexpr size_t game_days = 100;
-constexpr size_t final_goal = 2000;
-
-int main() {
-
-    return 0;
-}
