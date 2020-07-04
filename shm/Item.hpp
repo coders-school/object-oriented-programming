a5bccc88b70a65df11c703ddf2acb51d49536e28
Adam Mierzwiak <5563693+adamvm@users.noreply.github.com>
2020-07-04 06:29:28 +0200

    Add testing for Item class

--- shm/Item.hpp
+++ shm/Item.hpp
@@ -23,11 +23,11 @@ public:
     [[nodiscard]] size_t GetPrice() const override;
     [[nodiscard]] size_t GetAmount() const override;
     [[nodiscard]] size_t GetBasePrice() const override;
+
     Item& operator+=(size_t amount) override;
-
     Item& operator-=(size_t amount) override;
-    bool operator==(const Cargo& cargo) const override;
 
+    bool operator==(const Cargo& cargo) const override;
     bool operator!=(const Cargo& cargo) const override;
 
     [[nodiscard]] Item::Rarity GetRarity() const;
