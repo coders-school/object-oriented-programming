e4cc9e488ad6ac35343505ef471d0860bb2ead5b
Jakub Cieplinski <cieplinski.jakubsp@gmail.com>
2020-07-04 18:07:19 +0200

    Fixed variable names

--- shm/Player.cpp
+++ shm/Player.cpp
@@ -35,10 +35,10 @@ size_t Player::countAvailableSpace() {
 void Player::buyCargo(std::shared_ptr<Cargo> cargo, size_t amount, size_t price) {
     ship_->load(cargo);
     money_ -= price;
-    avaliableSpace_ -= amount;
+    availableSpace_ -= amount;
 }
 void Player::sellCargo(std::shared_ptr<Cargo> cargo, size_t amount, size_t price) {
     ship_->unload(cargo.get());
     money_ += price;
-    avaliableSpace_ += amount;
+    availableSpace_ += amount;
 }
