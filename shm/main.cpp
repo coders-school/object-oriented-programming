f24303c60584bab3fffa91b175a3a56366c8c48a
Marcinati <marcin.kapusta.pk@gmail.com>
2020-07-04 19:52:00 +0200

    startGame to start game

--- shm/main.cpp
+++ shm/main.cpp
@@ -5,7 +5,8 @@ constexpr size_t GAME_DAYS = 20;
 constexpr size_t FINAL_GOAL = 5000;
 
 int main() {
-
     Game game(START_MONEY, GAME_DAYS, FINAL_GOAL);
+    game.startGame();
+    
     return 0;
 }
