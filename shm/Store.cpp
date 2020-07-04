a1be2972db46a745ff098863a07f544df9e56e9c
Kuba C <jakub.cieplinski@outlook.com>
2020-07-04 20:42:37 +0200

    Removed unnecessary include

--- shm/Store.cpp
+++ shm/Store.cpp
@@ -1,16 +1,13 @@
 #include "Store.hpp"
 #include <algorithm>
-#include <iostream>
 
 Response Store::buy(std::shared_ptr<Cargo> cargo, size_t amount, Player* player) {
     const size_t price = amount * cargo->getBasePrice();
-    if (cargo -> getAmount() < amount){
+    if (cargo->getAmount() < amount) {
         return Response::lack_of_cargo;
-    }
-    else if (player -> getMoney() < price){
+    } else if (player->getMoney() < price) {
         return Response::lack_of_money;
-    }
-    else if (player -> getAvailableSpace() < amount){
+    } else if (player->getAvailableSpace() < amount) {
         return Response::lack_of_space;
     }
     *cargo += amount;
@@ -20,7 +17,7 @@ Response Store::buy(std::shared_ptr<Cargo> cargo, size_t amount, Player* player)
 
 Response Store::sell(std::shared_ptr<Cargo> cargo, size_t amount, Player* player) {
     const size_t price = amount * cargo->getPrice();
-    *cargo -= amount; 
+    *cargo -= amount;
     player->sellCargo(cargo, amount, price);
     return Response::done;
 }
@@ -29,8 +26,7 @@ void Store::nextDay() {
     //Doing nothing for now. Interface purposes
 }
 
-Store::Store(std::shared_ptr<Time> &time)
-    : time_(time)
-{
+Store::Store(std::shared_ptr<Time>& time)
+    : time_(time) {
     time_->addObserver(this);
 }
