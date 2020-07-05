19bdb27b774442ba226fd5e1ee020abfe8ca221b
Marcinati <marcin.kapusta.pk@gmail.com>
2020-07-05 20:30:45 +0200

    sort members

--- shm/game.hpp
+++ shm/game.hpp
@@ -23,10 +23,10 @@ private:
     size_t days_ {0};
     const size_t finalGoal_ {0};
     size_t currentDay_ {0};
-    std::shared_ptr<Map> map_;
     std::shared_ptr<Ship> ship_;
     std::shared_ptr<Player> player_;
     std::shared_ptr<Time> time_;
+    std::shared_ptr<Map> map_;
     Time* gameTime;
 
     void printTrail();
