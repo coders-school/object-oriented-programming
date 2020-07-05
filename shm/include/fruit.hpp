2aaeda0cd5c4c81b48601d25f9abb54b371e8cf0
jakjan95 <jakubjanusz95@gmail.com>
2020-07-05 10:43:49 +0200

    getPrice() debugged for Fruit class

--- shm/include/fruit.hpp
+++ shm/include/fruit.hpp
@@ -8,7 +8,9 @@ public:
     Fruit(const std::string& name, size_t amount, double basePrice, size_t timeToRotten);
     ~Fruit() override {}
 
-    double getPrice() const override { return getBasePrice() - 0.09 * static_cast<double>(getTimeToRotten()); }
+    double getPrice() const override {
+        return getBasePrice() * static_cast<double>((getTimeToRotten()) / (getTimeToRotten() + timeElapsed_));
+    }
     std::string getName() const override { return name_; }
     size_t getAmount() const override { return amount_; }
     double getBasePrice() const override { return basePrice_; }
@@ -26,19 +28,3 @@ private:
     size_t timeElapsed_ = 0;
     size_t timeToRotten_;
 };
-
-//DryFruit?
-// class DryFruit : public Fruit {
-//     std::string getName() const override { return "Dry Fruit"; }
-//     double getPrice() const override {
-//         return 3 * basePrice_ * (1 / std::exp(timeToRotten_));}
-//     DryFruit& operator--() override {
-//         if (++counter_ == 10) {
-//             ++time_elapsed_;
-//             counter_ = 0;
-//         }
-//         return *this;
-//     }
-// private:
-//     size_t counter_{0};
-// };
