f549620093eb0b039bda1000b5419c92102faa17
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-07-05 17:29:45 +0200

    Tests added

--- shm/test/test.cpp
+++ shm/test/test.cpp
@@ -1,6 +1,8 @@
 #include "gtest/gtest.h"
 
+#include "fruit.hpp"
 #include "ship.hpp"
+#include "store.hpp"
 
 constexpr int SHIP_MAX_CREW = 20;
 constexpr int SHIP_SPEED = 666;
@@ -11,4 +13,11 @@ TEST(ShipTest, ShouldInitShip) {
     EXPECT_EQ(ship.getMaxCrew(), SHIP_MAX_CREW);
     EXPECT_EQ(ship.getSpeed(), SHIP_SPEED);
     EXPECT_EQ(ship.getId(), SHIP_ID);
+}
+TEST(StreamTest, ShoutOutputStore) {
+    Store store;
+    Fruit fruit("apple", 50, 8, 99);
+    store.addCargo(&fruit);
+
+    std::cout << store;
 }
