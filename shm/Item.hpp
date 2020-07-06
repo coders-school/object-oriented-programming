94f5cfc6d539b1ce39413e81173855da3a3be262
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-07-06 15:16:12 +0200

    changed order in Item class and constructor added

--- shm/Item.hpp
+++ shm/Item.hpp
@@ -9,20 +9,21 @@ public:
         legendary = 10
     };
 
+private:
+    const Rarity rarity_ = Rarity::common;
+
+public:
+    Item(const std::string& name, size_t amount, size_t basePrice, Rarity rarity);
+
     //override from Cargo
     std::string getName() const override { return name_; }
     size_t getAmount() const override { return amount_; }
     size_t getBasePrice() const override { return basePrice_; }
     size_t getPrice() const override { return basePrice_ * static_cast<int>(rarity_); }
-
-    Rarity getRarityLevel() const { return rarity_; }
-
     Cargo& operator+=(const size_t amount) override;
     Cargo& operator-=(const size_t amount) override;
-    
     bool operator==(const Cargo& cargo) const override;
     bool operator!=(const Cargo& cargo) const override;
 
-private:
-    const Rarity rarity_ = Rarity::common;
+    Rarity getRarityLevel() const { return rarity_; }
 };
