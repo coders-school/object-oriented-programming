0c6d317ae6d01a50d24db7c6c4d21688924a749a
kszytko <krzysiek.szytko@gmail.com>
2020-07-07 20:05:58 +0200

    Added time into Cargo constructors

--- shm/Cargo.cpp
+++ shm/Cargo.cpp
@@ -1,7 +1,13 @@
 #include "Cargo.hpp"
 
-Cargo::Cargo(const std::string& name, size_t amount, size_t basePrice)
+Cargo::Cargo(const std::string& name, size_t amount, size_t basePrice, Time* time)
     : _name(name),
       _amount(amount),
-      _basePrice(basePrice) {
+      _basePrice(basePrice),
+      _time(time) {
+    _time->addObserver(this);
+}
+
+Cargo::~Cargo() {
+    _time->removeObserver(this);
 }
