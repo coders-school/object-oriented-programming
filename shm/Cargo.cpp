4a5cec757bb8a0c2f6a272ef54e4a60e086257c1
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-07-11 16:46:18 +0200

    removed implementations of pure virtual operators += -= == != from Cargo class as they are overridden in derived classes

--- shm/Cargo.cpp
+++ shm/Cargo.cpp
@@ -3,25 +3,3 @@
 Cargo::Cargo(const std::string& name, size_t amount, size_t basePrice)
     : name_(name), amount_(amount), basePrice_(basePrice) {
 }
-
-Cargo& Cargo::operator+=(const size_t amount) {
-    amount_ += amount;
-    return *this;
-}
-
-Cargo& Cargo::operator-=(const size_t amount) {
-    if (amount > amount_) {
-        amount_ = 0;
-        return *this;
-    }
-    amount_ -= amount;
-    return *this;
-}
-
-bool Cargo::operator==(const Cargo& cargo) const {
-    return (name_ == cargo.name_);
-}
-
-bool Cargo::operator!=(const Cargo& cargo) const {
-    return (name_ != cargo.name_);
-}
