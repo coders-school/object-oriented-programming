12e8f68f504c867d2781462f35cf699940fab67b
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-07-12 20:27:51 +0200

    New methods: travel(), getDistanceToIsland(), getCurrentPosition(); code formatting issues

--- shm/inc/map.hpp
+++ shm/inc/map.hpp
@@ -8,10 +8,13 @@ class Map {
 public:
     Map();
     virtual ~Map();
-    Island *getIsland(const Coordinates& coordinates);
+    Island* getIsland(const Coordinates& coordinates);
     void printMap();
+    void travel(Island* island);
+    size_t getDistanceToIsland(Island* destination) const;
+    Island* getCurrentPosition() const;
 
 private:
     std::vector<Island> vectorOfIslands_;
-    Island *currentPosition_;
+    Island* currentPosition_;
 };
