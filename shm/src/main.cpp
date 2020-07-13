686d19069af88781ddda878862344d4f86f4d45d
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-07-13 10:30:33 +0200

    Descriptive constants used to launch the game (still commented)

--- shm/src/main.cpp
+++ shm/src/main.cpp
@@ -1,7 +1,11 @@
 #include "game.hpp"
 
+constexpr size_t startMoney = 500;
+constexpr size_t gameDays = 365;
+constexpr size_t finalGoal = 9999;
+
 int main() {
-    // Game game(999, 360, 9999);
+    // Game game(startMoney, gameDays, finalGoal);
     // game.startGame();
 
     return 0;
