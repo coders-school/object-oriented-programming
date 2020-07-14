d35f2157b28fd9dc7b4a3fcae739b678f64a31c8
PiotrHCpp <piotr.hoffner.wroc@gmail.com>
2020-07-14 15:19:35 +0200

    added getNameMethodShouldReturnCargoName Cargo test

--- shm/test.cpp
+++ shm/test.cpp
@@ -24,12 +24,18 @@ protected:
     const std::string secondCommodity = "nuts";
 };
 
-TEST_F(CargoTests, EqualOperatorShouldReturnTrue) {
+TEST_F(CargoTests, equalOperatorShouldReturnTrue) {
     EXPECT_TRUE(*cargo1 == *cargo2);
     EXPECT_TRUE(*cargo2 == *cargo1);
 }
 
-TEST_F(CargoTests, EqualOperatorShouldReturnFalse) {
+TEST_F(CargoTests, equalOperatorShouldReturnFalse) {
     EXPECT_FALSE(*cargo1 == *cargo3);
     EXPECT_FALSE(*cargo3 == *cargo1);
+}
+
+TEST_F(CargoTests, getNameMethodShouldReturnCargoName) {
+    EXPECT_EQ(firstCommodity, cargo1->getName());
+    EXPECT_EQ(firstCommodity, cargo2->getName());
+    EXPECT_EQ(secondCommodity, cargo3->getName());
 }
