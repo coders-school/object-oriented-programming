1364b9358dae07bccd7f44da0646b26872208106
PiotrHCpp <piotr.hoffner.wroc@gmail.com>
2020-07-16 17:21:46 +0200

    introduced Map::islandGenerator()

--- shm/Map.cpp
+++ shm/Map.cpp
@@ -1,1 +1,26 @@
 #include "Map.hpp"
+
+#include <algorithm>
+#include <random>
+
+Map::Map() : sizeX_(10), sizeY_(10) {
+    islandGenerator(10);
+}
+Map::Map(size_t sizeX, size_t sizeY, size_t islandAmount)
+    : sizeX_(sizeX), sizeY_(sizeY) {
+    islandGenerator(islandAmount);
+}
+
+void Map::islandGenerator(size_t amount) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+
+    std::vector<int> temporaryMap_(sizeX_ * sizeY_);
+    std::iota(std::begin(temporaryMap_), std::end(temporaryMap_), 0);
+    std::shuffle(std::begin(temporaryMap_), std::end(temporaryMap_), gen);
+
+    for(size_t i = 0; i < amount; ++i) {
+        islandsArrangement_.push_back(Island(temporaryMap_[i] % sizeX_,
+                                            (temporaryMap_[i] / sizeX_)  % sizeY_));
+    }
+}
--- shm/Map.hpp
+++ shm/Map.hpp
@@ -7,8 +7,14 @@
 
 class Map {
 public:
+    Map();
+    Map(size_t sizeX, size_t sizeY, size_t islandAmount);
 
 private:
+    void islandGenerator(size_t amount);
+
+    size_t sizeX_;
+    size_t sizeY_;
     std::vector<Island> islandsArrangement_;
     std::shared_ptr<Island> currentPosition_{};
 };
