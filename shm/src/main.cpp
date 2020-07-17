8f806f8976fce3df91ec54bdc9dcf4606680f119
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-07-17 15:28:25 +0200

    Main driver code disabled (for cmake tests)

--- shm/src/main.cpp
+++ shm/src/main.cpp
@@ -5,8 +5,8 @@ constexpr size_t gameDays = 365;
 constexpr size_t finalGoal = 9999;
 
 int main() {
-    Game game(startMoney, gameDays, finalGoal);
-    game.startGame();
+    // Game game(startMoney, gameDays, finalGoal);
+    // game.startGame();
 
     return 0;
 }
