f644c633b31e7ca1f8a92a0f0a1dc4def646a9b7
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-07-17 14:07:09 +0200

    setAmount() method set as pure virtual in Cargo class

--- shm/inc/cargo.hpp
+++ shm/inc/cargo.hpp
@@ -20,10 +20,10 @@ public:
     virtual size_t getAmount() const = 0;
     virtual size_t getBasePrice() const = 0;
 
-    void setAmount(const size_t amount) { amount_ = amount; }
+    virtual void setAmount(const size_t amount) = 0;
     virtual std::unique_ptr<Cargo> clone() = 0;
 
- protected:
+protected:
     std::string name_;
     size_t amount_;
     size_t basePrice_;
