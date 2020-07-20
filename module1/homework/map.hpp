9f1865298719d643154c438f88cd281ea6e505d2
Macko <62703601+MaciejSalamonski@users.noreply.github.com>
2020-07-20 23:13:51 +0200

    Update map.hpp

--- module1/homework/map.hpp
+++ module1/homework/map.hpp
@@ -5,21 +5,12 @@
 #include "Coordinates.hpp"
 #include "Island.hpp"
 
-
-constexpr size_t numOfIslands = 10;
-constexpr size_t mapX = 100;
-constexpr size_t mapY = 100;
-
-
-
-
 class Map {
     std::vector<Island> islands_;
     Island* currentPosition_;
 
 public:
     Map();
-    
-    Island* getIsland(const Island::Coordinates& coordinates);
-    void creaIsland(size_t numb);
+    Island* getIsland(const Island::Coordinates&);
+    void creaIsland(size_t);
 };
