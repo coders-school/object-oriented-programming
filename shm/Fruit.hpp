9c00bf72bab9101295d55dd13eabe5ea4f1c3ff6
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-07-23 01:14:39 +0200

    changed name

--- shm/Fruit.hpp
+++ shm/Fruit.hpp
@@ -2,10 +2,11 @@
 
 #include "Cargo.hpp"
 
+const size_t expirationDays{10};
+
 class Fruit : public Cargo {
 private:
     size_t daysToRot_{10};
-    const size_t expirationDays_{10};
 
 public:
     Fruit(const std::string& name, size_t amount, size_t basePrice, size_t daysToRot);
@@ -18,7 +19,7 @@ public:
     size_t getAmount() const override { return amount_; }
     size_t getBasePrice() const override { return basePrice_; }
     size_t getPrice() const override {
-        return static_cast<size_t>(basePrice_ * daysToRot_ / expirationDays_);
+        return static_cast<size_t>(basePrice_ * daysToRot_ / expirationDays);
     }
     Cargo& operator+=(const size_t amount) override;
     Cargo& operator-=(const size_t amount) override;
@@ -28,5 +29,5 @@ public:
     Fruit& operator--();
 
     size_t getDaysToRot() const { return daysToRot_; }
-    size_t getExpirationDays() const { return expirationDays_; }
+    size_t getExpirationDays() const { return expirationDays; }
 };
