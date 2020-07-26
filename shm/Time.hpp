7f89cd7932acbb5bd9c2100d814712e994a0d332
Adam <5563693+adamvm@users.noreply.github.com>
2020-07-26 06:49:01 +0200

    Add class Time (#43)

---
+++ shm/Time.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Observer.hpp"
+#include <vector>
+
+class Time {
+public:
+    Time() = default;
+
+    void addObserver(Observer *obs);
+    void removeObserver(Observer *obs);
+    [[nodiscard]] size_t getElapsedTime() const {return time_elapsed_; }
+    Time& operator++();
+
+private:
+    size_t time_elapsed_{};
+    std::vector<Observer*> observers_;
+
+    void notify();
+};
