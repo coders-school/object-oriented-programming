8379db14d54c486e23a4737003511a4f2ce850f1
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-08-15 23:51:41 +0200

    added isEmpty method in StockManagement class to check if Stock's vector is empty

--- shm/StockManagement.hpp
+++ shm/StockManagement.hpp
@@ -12,4 +12,5 @@ public:
     void removeCargoFromStock(const CargoPtr& cargo, size_t amount);
     void addCargoToStock(const CargoPtr& cargo, size_t amount);
     CargoPtr getCargo(size_t index);
+    bool isEmpty() const { return stock_.size() == 0; };
 };
