2e46665b912890082b34f870b3553376dd4ba8e5
MaciejSalamonski <macieksalamonski@gmail.com>
2020-08-17 00:24:34 +0200

    class Store

--- module1/homework/Island.cpp
+++
@@ -1,8 +0,0 @@
-#include "Island.hpp"
-
-Island::Island(uint16_t positionX, uint16_t positionY)
-    : position_(Coordinates(positionX, positionY)) {}
-
-Coordinates Island::getPosition() const {
-    return position_;
-}
