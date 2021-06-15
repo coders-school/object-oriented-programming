694e3727b47185a6040add431e458aac194aebf8
Piotr Rogalski <progalski89@gmail.com>
2021-06-15 21:42:53 +0200

    player class added with some features, will not compile due to lack of Ship and Cargo classes

---
+++ shm/player.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <memory>
+
+// Forward declaration, include Ship/Cargo class once they are ready
+class Ship;
+class Cargo;
+
+class Player {
+    std::unique_ptr<Ship> ship_;
+    size_t money_;
+    size_t availableSpace_;
+
+    void calculateAvailableSpace();
+
+public:
+    Player(std::unique_ptr<Ship>& ship, const size_t& money, const size_t availableSpace);
+    const std::unique_ptr<Ship>& getShip() const;
+    size_t getMoney() const;
+    size_t getAvailableSpace() const;
+    size_t getSpeed() const;
+    Cargo* getCargo(size_t index) const;
+};
