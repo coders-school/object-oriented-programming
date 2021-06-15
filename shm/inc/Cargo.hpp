8869b54f76519466010b246cf8a27e77c6e37708
lukasz-kukulka <qqla83@gmail.com>
2021-06-15 21:53:31 +0100

    Compelted task2 getters and c-tor Cargo

--- shm/inc/Cargo.hpp
+++ shm/inc/Cargo.hpp
@@ -1,8 +1,15 @@
 #pragma once
+
+#include <string>
 class Cargo {
 public:
-
-    bool operator==(const Cargo&);
+    Cargo(std::string, size_t, size_t);
+    std::string getName() const { return name_; }
+    size_t getAmount() const { return amount_; }
+    size_t getBestPrice() const { return basePrice_; }
 
 private:
+    const std::string name_ { }; 
+    size_t amount_ { 0 };
+    size_t basePrice_ { 0 }; 
 };
--- shm/src/Cargo.cpp
+++ shm/src/Cargo.cpp
@@ -1,3 +1,7 @@
-bool Cargo::operator==(const Cargo& cargo){
-    return cargo.name_ == name_ ? true : false;
-}
+#include "Cargo.hpp"
+
+Cargo::Cargo(std::string name, size_t amount, size_t basePrice) 
+    : name_(name)
+    , amount_(amount)
+    , basePrice_(basePrice)
+{}
