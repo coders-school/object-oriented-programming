094f880f6167a7011c37c1788a3df29c12307fd2
lukasz-kukulka <qqla83@gmail.com>
2021-06-15 21:35:56 +0100

    Coplete task 1

--- shm/src/Cargo.cpp
+++ shm/src/Cargo.cpp
@@ -1,15 +1,3 @@
-#include "../inc/Cargo.hpp"
-
-Cargo& Cargo::operator+=(const size_t value){
-    amount_ += value;
-    return *this;
-}
-
-Cargo& Cargo::operator-=(const size_t value){
-    amount_ -= value;
-    return *this;
-}
-
-bool operator==(const Cargo& cargo){
-    //if(name_ == cargo.)
+bool Cargo::operator==(const Cargo& cargo){
+    return cargo.name_ == name_ ? true : false;
 }
