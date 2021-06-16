6f843988e633f9bb1f74b4227a01206606eb645c
lukasz-kukulka <qqla83@gmail.com>
2021-06-16 10:42:22 +0100

    Afret Mariusz code review

--- shm/inc/Cargo.hpp
+++ shm/inc/Cargo.hpp
@@ -1,15 +1,20 @@
 #pragma once
-
 #include <string>
+
 class Cargo {
 public:
-    Cargo(std::string, size_t, size_t);
+    Cargo(const std::string&, size_t, size_t);
+
+    Cargo& operator+=(const size_t);
+    Cargo& operator-=(const size_t);
+    bool operator==(const Cargo&) const;
+
     std::string getName() const { return name_; }
     size_t getAmount() const { return amount_; }
     size_t getBestPrice() const { return basePrice_; }
 
 private:
-    const std::string name_ { }; 
-    size_t amount_ { 0 };
-    size_t basePrice_ { 0 }; 
+    const std::string name_; 
+    size_t amount_ {};
+    size_t basePrice_ {}; 
 };
--- shm/src/Cargo.cpp
+++ shm/src/Cargo.cpp
@@ -1,7 +1,26 @@
 #include "Cargo.hpp"
+#include <iostream>
 
-Cargo::Cargo(std::string name, size_t amount, size_t basePrice) 
+Cargo::Cargo(const std::string& name, size_t amount, size_t basePrice) 
     : name_(name)
     , amount_(amount)
     , basePrice_(basePrice)
 {}
+
+Cargo& Cargo::operator+=(const size_t amount) {
+    amount_ += amount;
+    return *this;
+}
+
+Cargo& Cargo::operator-=(const size_t amount) {
+    if (amount <= amount_) {
+        amount_ -= amount;
+    } else {
+        std::cerr << "Amount of cargo can't be under 0\n";
+    }
+    return *this;
+}
+
+bool Cargo::operator==(const Cargo& cargo) const {
+    return cargo.name_ == name_;
+}
