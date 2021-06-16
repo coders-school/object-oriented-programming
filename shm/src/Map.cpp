8022ca52c99feee8d4f91f83b5c4222b5ec287f8
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-06-16 11:07:59 +0200

    Add Mersenne Twister random generator with uniform distribution

--- shm/src/Map.cpp
+++ shm/src/Map.cpp
@@ -1,5 +1,14 @@
 #include "Map.hpp"
 
+#include <random>
+
+constexpr int COORDINATE_MIN = 0;
+constexpr int COORDINATE_MAX = 99;
+
 Map::Map() {
-    
+    std::random_device rd;
+    std::mt19937 generator(rd);
+    std::uniform_int_distribution<> distribution{
+        COORDINATE_MIN, COORDINATE_MAX
+    };
 }
