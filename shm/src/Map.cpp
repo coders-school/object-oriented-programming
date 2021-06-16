1867b65f8ae12e73239aaa769992644ab23fe1bb
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-06-16 11:38:34 +0200

    Add an unique islands generator

--- shm/src/Map.cpp
+++ shm/src/Map.cpp
@@ -1,9 +1,11 @@
 #include "Map.hpp"
 
+#include <algorithm>
 #include <random>
 
 constexpr int COORDINATE_MIN = 0;
 constexpr int COORDINATE_MAX = 99;
+constexpr int ISLANDS_COUNT = 10;
 
 Map::Map() {
     std::random_device rd;
@@ -11,4 +13,20 @@ Map::Map() {
     std::uniform_int_distribution<> distribution{
         COORDINATE_MIN, COORDINATE_MAX
     };
+
+    while (vectorOfIslands_.size() != ISLANDS_COUNT) {
+        auto generatedIslandCoords{
+            Island::Coordinates(distribution(generator),
+                                distribution(generator))
+        };
+        auto result = std::find_if(vectorOfIslands_.begin(),
+                                   vectorOfIslands_.end(),
+                                   [&](const Island& island) {
+                                       return island.getCoordinates() ==
+                                               generatedIslandCoords;
+                                   });
+        if (result == vectorOfIslands_.end()) {
+            vectorOfIslands_.push_back(Island(generatedIslandCoords));
+        }
+    }
 }
