7f5c012c940b0cce7b20faa236661892ee8999bf
lukasz-kukulka <qqla83@gmail.com>
2021-06-16 10:31:43 +0100

    Afret Mariusz code review part 2

--- shm/inc/Cargo.hpp
+++ shm/inc/Cargo.hpp
@@ -8,7 +8,7 @@ public:
     bool operator==(const Cargo&) const;
 
 private:
-    const std::string name_ { }; 
+    const std::string name_; 
     size_t amount_ { 0 };
     size_t basePrice_ { 0 }; 
 };
--- shm/src/Cargo.cpp
+++ shm/src/Cargo.cpp
@@ -1,14 +1,14 @@
 #include "../inc/Cargo.hpp"
 #include <iostream>
 
-Cargo& Cargo::operator+=(const size_t value) {
-    amount_ += value;
+Cargo& Cargo::operator+=(const size_t amount) {
+    amount_ += amount;
     return *this;
 }
 
-Cargo& Cargo::operator-=(const size_t value) {
-    if (value <= amount_) {
-        amount_ -= value;
+Cargo& Cargo::operator-=(const size_t amount) {
+    if (amount <= amount_) {
+        amount_ -= amount;
     } else {
         std::cerr << "Amount of cargo can't be under 0\n";
     }
