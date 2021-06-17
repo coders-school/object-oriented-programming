cc9a5e9af46c4c0d0be21ab361939d0b92336b3f
Piotr Rogalski <progalski89@gmail.com>
2021-06-17 23:33:18 +0200

    Added map/insland test and fixed minor bugs

--- shm/main.cpp
+++ shm/main.cpp
@@ -1,5 +1,7 @@
 #include "Player.hpp"
 #include "Cargo.hpp"
+#include "Map.hpp"
+#include "Island.hpp"
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -38,19 +40,42 @@ std::unique_ptr<Cargo> generateCargo() {
     return std::move(ptr);
 }
 
-int main() {
-    srand(time(0));
+void testCargoShipPlayer() {
     constexpr size_t testCases {10};
+    constexpr size_t testShipCapacity {100};
     std::vector<std::unique_ptr<Cargo>> cargoVec;
-    cargoVec.reserve(100);
+    cargoVec.reserve(testShipCapacity);
     for(size_t i = 0; i < testCases; ++i) {
-        for(size_t i = 0; i < rand() % 100; ++i) {
+        size_t cargoAmount = rand() % testShipCapacity;
+        for(size_t i = 0; i < cargoAmount; ++i) {
             cargoVec.push_back(generateCargo());
         }
-        auto pirateShip = std::make_unique<Ship>(100, 40, 10, "The Adventure Galley Pirate Ship", 0, std::move(cargoVec));
+        auto pirateShip = std::make_unique<Ship>(testShipCapacity, 40, 10, "The Adventure Galley Pirate Ship", 0, std::move(cargoVec));
+        cargoVec.clear();
         Player pirate(std::move(pirateShip), 1000);
-        std::cout << "\n\n--- TEST CASE ---\n";
+        std::cout << "\n\n--- CARGO/SHIP/PLAYER TEST ---\n";
         pirate.printCargoManifest();
     }
+}
+
+void testIslandMap() {
+    constexpr size_t testCases {10};
+    for(size_t i = 0; i < testCases; ++i) {
+        Map map;
+        auto mapVec = map.getIslandVec();
+        size_t num {1};
+        std::cout << "\n\n--- MAP/ISLAND TEST ---\n";
+        for(const auto& island : mapVec) {
+            std::cout << num++ << ". ";
+            std::cout << island.getPosition().toString();
+            std::cout << '\n';
+        }
+    }
+}
+
+int main() {
+    srand(time(0));
+    testCargoShipPlayer();
+    testIslandMap();
     return 0;
 }
