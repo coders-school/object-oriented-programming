c663b0f84d65f3cabb92032103915388d665de29
Artur Michałowski <artur.michalowski93@gmail.com>
2021-06-19 15:47:19 +0200

    Changed sth...

--- shm/src/Player.cpp
+++ shm/src/Player.cpp
@@ -7,7 +7,7 @@ Player::Player(std::shared_ptr<Ship> ship, size_t money, size_t availableSpace)
 size_t Player::getSpeed() const {
     return ship_->getSpeed();
 };
-
+  
 std::shared_ptr<Cargo> Player::getCargo(size_t index) const {
     return ship_->getCargo(index);
 }
