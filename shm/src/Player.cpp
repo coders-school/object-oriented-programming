c212766ae42fae936b26c5fb203ef0c236e46be9
BarTes8 <barbara.tessema@gmail.com>
2021-06-20 20:32:29 +0200

    fixup! added return to countAvailableSpace()

--- shm/src/Player.cpp
+++ shm/src/Player.cpp
@@ -20,5 +20,5 @@ size_t Player::countAvailableSpace() const {
                     [](size_t sum, const auto& cargo) {
                         return sum += cargo->getAmount();
                     });
-    return ship->getCapcity() - usedSpace;
+    return ship_->getCapcity() - usedSpace;
 }
