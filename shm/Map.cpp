3f5d8f7cb69bd846087d6e86601b22495ab1789f
Jakub Kubalka <jk.kubalka@gmail.com>
2021-06-22 15:21:37 +0200

    Added getPosition, Initialization, default constructor, some additional changes

--- shm/Map.cpp
+++ shm/Map.cpp
@@ -1,8 +1,11 @@
 #include "Map.hpp"
 
-Map::Map(Island island, Island* currentPosition)
-    : currentPostion_(currentPostion) 
+Map::Map(Island* currentPosition)
+    : currentPostion_(currentPosition)
     {}
-void Map::initialize () {
-    
+void Map::initializeMap (const Island& island) {
+    islands_.push_back(island);
+}
+Island* Map::getPosition() {
+    return currentPostion_;
 }
--- shm/Map.hpp
+++ shm/Map.hpp
@@ -1,16 +1,17 @@
 #pragma once
-#include <string>
 #include <vector>
 #include <memory>
 #include <functional>
 
 class Map {
 public:
-    Map(Island island, Island* currentPosition)
+    Map() = default;
+    Map(Island* currentPosition) //I dont know, if i want to fill vector of islands, i need to put island into constructor, or filling the vector will be done only by function
         : currentPostion_(currentPosition)
         {}
-    virtual void initialize(island);
+    virtual void initializeMap(const Island& island);
+    Island* const getPosition();
 private:
     std::vector<Island&> islands_; //I dont know if this vector is going to be the only place where we are collecting islands. If its the only way to somehow "remember" islands, then pointer is pointless (hehe) 
-    Island* currentPostion_;                        
+    Island* currentPostion_;
 };
