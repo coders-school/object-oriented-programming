1613844f0fc5f48db2e0270529112bf2f4700e3d
Kacu <kacper.kaleta@scythe-studio.com>
2021-06-24 19:36:52 +0200

    moved Coordinates into Island

--- shm/Island.hpp
+++ shm/Island.hpp
@@ -2,11 +2,29 @@
 
 #include "Coordinates.hpp"
 
-class Island {
-	Coordinates position_;
+struct Island {
+	struct Coordinates {
+		int positionX;
+		int positionY;
+
+		Coordinates(const int x, const int y):
+			positionX(x),
+			positionY(y)
+		{}
+
+		bool operator==(const Coordinates& coordinates) {
+			return (positionX == coordinates.positionX && positionY == coordinates.positionY);
+		}
+	};
 
 public:
+	Island(const Coordinates& coordinates):
+		position_(coordinates)
+	{}
+
 	Coordinates getPosition() const { return position_; }
 
+private:
+	Coordinates position_;
 
-}
+};
