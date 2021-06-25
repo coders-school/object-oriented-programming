80f9fc03ccc38df5bdb566ceae0033ea6b400920
kondorski <thegreatvaultofknowledge@gmail.com>
2021-06-25 21:37:53 +0200

    player refactor

--- shm/player.cpp
+++ shm/player.cpp
@@ -1,5 +1,27 @@
 #include "player.hpp"
 
-Player::Player(std::unique_ptr<Ship> ship, int money, int availableSpace)
-    : ship_(ship_.get()), money_(money), availableSpace_(availableSpace)
+Player::Player(std::unique_ptr<Ship>& ship, int money, int availableSpace)
+    : ship_(std::move(ship_))
+    , money_(money)
+    , availableSpace_(availableSpace)
     {}
+
+std::unique_ptr<Ship>& Player::getShip() { 
+    return ship_; 
+}
+
+size_t Player::getMoney() { 
+    return money_; 
+}
+
+size_t Player::getAvailableSpace () { 
+    return availableSpace_; 
+}
+
+size_t Player::getSpeed() const { 
+    return ship_->getSpeed(); 
+}
+
+Cargo* Player::getCargo(size_t index) const {
+    return ship_->getCargo()[index];
+}
--- shm/player.hpp
+++ shm/player.hpp
@@ -1,21 +1,22 @@
 #pragma once
+
 #include "ship.hpp"
 #include "cargo.hpp"
 #include <memory>
 
 class Player {
-public:
-    std::unique_ptr<Ship> getShip () { ship_.get(); }
-    int getMoney () { return money_; }
-    size_t getAvailableSpace () { return availableSpace_; }
-
-    size_t getSpeed() const { return ship_.get()->getSpeed(); }
-    
-
-    Player::Player(std::unique_ptr<Ship> ship, int money, int availableSpace);
-
 private:
-    std::unique_ptr<Ship> ship_ = std::make_unique<Ship>();
+    std::unique_ptr<Ship> ship_;
     int money_;
     size_t availableSpace_;
+
+public:
+    Player(std::unique_ptr<Ship>& ship, int money, int availableSpace);
+
+    std::unique_ptr<Ship>& getShip();
+    size_t getMoney();
+    size_t getAvailableSpace();
+
+    size_t getSpeed() const;
+    Cargo* getCargo(size_t index) const;
 };
