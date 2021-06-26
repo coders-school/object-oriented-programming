3c0dd7d5c5a5e2eb82a33457cad7389a2c76f97b
Katarzyna Strączyńska <katarzyna.a.straczynska@gmail.com>
2021-06-26 12:06:01 +0200

    Adding function contains

--- shm/Map.cpp
+++ shm/Map.cpp
@@ -1,1 +1,13 @@
 #include "Map.hpp"
+
+bool Map::contains(std::vector<Coordinates> vec, Coordinates c)
+{
+    if (std::find(vec.begin(), vec.end(), c) != vec.end())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
