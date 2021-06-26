a099870d005052f20e0ecd69d9b4e61975bba0cb
Katarzyna Strączyńska <katarzyna.a.straczynska@gmail.com>
2021-06-26 11:54:25 +0200

    Adding task 5

--- shm/Map.hpp
+++ shm/Map.hpp
@@ -1,12 +1,33 @@
 #pragma once
-
 #include <vector>
 #include "island.hpp"
+#include <random>
+#include <algorithm>
 
-class Map {
-    public:
+class Map
+{
+public:
+    Map()
+    {
+        islands_.reserve(10);
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> distrib(-100, 100);
 
-    private:
-    Island* current_pos_;    
-    std::vector <Island> islands_;
+        std::vector<Coordinates> cords;
+        for (int i = 0; i < 10; i++)
+        {
+            Coordinates c(distrib(gen), distrib(gen));
+            while (contains(cords, c))
+            {
+                c.SetPositionX(distrib(gen)).SetPositionY(distrib(gen));
+            }
+            islands_.push_back(c);
+        }
+    }
+
+private:
+    Island *current_pos_;
+    std::vector<Island> islands_;
+    bool contains(std::vector<Coordinates>, Coordinates);
 };
