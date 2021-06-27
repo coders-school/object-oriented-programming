c24beba6cc42c19a425307394d6c87c1767e047d
MalarzDawid <github@dawidmalarz.pl>
2021-06-27 14:57:03 +0200

    Add coordinates class

---
+++ shm/coordinates.cpp
@@ -0,0 +1,8 @@
+#include "coordinates.hpp"
+
+bool Coordinates::operator==(const Coordinates& other) const {
+    if (positionX_ == other.positionX_ and positionY_ == other.positionY_ ) {
+        return true;
+    }
+    return false;
+}
