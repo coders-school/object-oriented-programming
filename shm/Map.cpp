47df11f34925dc25dbec282b742f530417ec8cf4
pwiatr3 <pwiatr3@yahoo.pl>
2021-06-27 12:14:45 +0200

    task 5

--- shm/Map.cpp
+++ shm/Map.cpp
@@ -1,13 +1,35 @@
 #include "Map.hpp"
 
-bool Map::contains(std::vector<Coordinates> vec, Coordinates c)
-{
-    if (std::find(vec.begin(), vec.end(), c) != vec.end())
-    {
+Map::Map() {
+    islands_.reserve(ISLANDS_COUNT);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> distrib(COORDINATE_MIN , COORDINATE_MAX);
+
+    std::vector<Coordinates> cords;
+    for (int i = 0; i < ISLANDS_COUNT; i++)
+    {   
+        Coordinates c;
+        c.SetPositionX(distrib(gen));
+        c.SetPositionY(distrib(gen));
+
+        while (contains(cords, c))
+        {
+            c.SetPositionX(distrib(gen));
+            c.SetPositionY(distrib(gen));
+        }
+        islands_.push_back(Island (c));
+    }
+}
+void Map::DebugPrintIsland () {
+    for (auto &el : islands_) {
+        std::cout << el.GetPosition().GetPositionX() << " | " << el.GetPosition().GetPositionY() << '\n';
+    }
+}
+
+bool Map::contains(std::vector<Coordinates> vec, Coordinates c) {
+    if (std::find(vec.begin(), vec.end(), c) != vec.end()) {
         return true;
     }
-    else
-    {
-        return false;
-    }
+    return false;
 }
--- shm/Map.hpp
+++ shm/Map.hpp
@@ -5,45 +5,16 @@
 #include <algorithm>
 #include <utility>
 #include <iostream>
+#include "coordinates.hpp"
 
-class Map
-{
+constexpr size_t COORDINATE_MIN = 0;
+constexpr size_t COORDINATE_MAX = 99;
+constexpr size_t ISLANDS_COUNT = 10;
+
+class Map {
 public:
-    Map()
-    {
-        islands_.reserve(10);
-        std::random_device rd;
-        std::random_device rd2;
-        std::mt19937 gen(rd());
-        std::mt19937 gen2(rd2());
-        std::uniform_real_distribution<> distrib(-100, 100);
-
-
-        std::vector<Coordinates> cords;
-        for (int i = 0; i < 10; i++)
-        {
-            Coordinates c(distrib(gen), distrib(gen2));
-            // while (contains(cords, c))
-            // {
-            //     c.SetPositionX(distrib(gen)).SetPositionY(distrib(gen));
-            // }
-            islands_.push_back(Island (c));
-        }
-
-    }
-
-    void DebugPrintIsland (){
-        for (auto &el : islands_){
-            std::cout << el.GetPosition().GetPositionX() << " | " << el.GetPosition().GetPositionY() << '\n';
-        }
-    }
-/*
-    tworzymym obiekt Coordinates
-    sprawdzić czy taki obiekt istnieje w wektorze
-    jeśli istnieje to generujemy kolejny
-    jeśli nie to dodajemy
-*/
-
+    Map();
+    void DebugPrintIsland();
 private:
     Island *current_pos_;
     std::vector<Island> islands_;
