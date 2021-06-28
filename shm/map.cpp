54b9ed68443ece019ddae40f11dec3c2c51b186a
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-06-28 23:34:57 +0200

    zadanie 5 implementation

--- shm/map.cpp
+++ shm/map.cpp
@@ -1,20 +1,24 @@
+#include <random>
 #include "map.hpp"
 #include "island.hpp"
 
-Map::Map(Island* currentPosition)
-    : currentPostion_(currentPosition)
-    {}
-void Map::initializeMap (Island& island) { //I think there might be some problems that i am using not const reference
-    islands_.push_back(island);
+Map::Map() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> widthDistribution(0, maxWidth_);
+    std::uniform_int_distribution<> heightDistribution(0, maxHeight_);
+
+    islands_.reserve(10);
+    for (int i = 0; i < islandsQuantity_; ++i) {
+        Island tempIsland(widthDistribution(gen), heightDistribution(gen));
+        islands_.push_back(tempIsland);
+    }
 }
+
 Island* const Map::getPosition() {
    return currentPostion_;
 }
 
 Island* Map::getIsland(const Island::Coordinates& coordinate) {
-    //Ok, if i understand it correctly, std::find should work, because we have definiiton of ==. Source: https://stackoverflow.com/questions/42933943/how-to-use-lambda-for-stdfind-if
     return std::find(islands_.begin(), islands_.end(), coordinate);
-    //std::find_if(islands_.begin(), islands_.end(), [&coordinate](const Island& island) {
-    //    return island::Coordinates == coordinate;
-    //})
 }
--- shm/map.hpp
+++ shm/map.hpp
@@ -6,14 +6,13 @@
 
 class Map {
 public:
-    Map() = default;
-    Map(Island* currentPosition) //I dont know, if i want to fill vector of islands, i need to put island into constructor, or filling the vector will be done only by function
-        : currentPostion_(currentPosition)
-        {}
-    void initializeMap(Island& island);
+    Map();
     Island* const getPosition();
     Island* getIsland(const Island::Coordinates& coordinate);
 private:
-    std::vector<Island&> islands_; //I dont know if this vector is going to be the only place where we are collecting islands. If its the only way to somehow "remember" islands, then pointer is pointless (hehe) 
+    const int maxWidth_ = 99;
+    const int maxHeight_ = 99;
+    const int islandsQuantity_ = 10;
+    std::vector<Island> islands_;
     Island* currentPostion_;
 };
