fa1efed545c402132e1c73e11de466d346c22065
Marcin$ <marcin.s10335@gmail.com>
2021-06-28 21:09:24 +0200

    Add Task6

--- shm/map.cpp
+++ shm/map.cpp
@@ -1,9 +1,10 @@
 #include "map.hpp"
+#include <algorithm>
 #include <functional>
 #include <iostream>
 #include <map>
-#include <random>
 #include <memory>
+#include <random>
 
 map::map() {
     auto returnPaired = generateIslandPosition();
@@ -24,7 +25,7 @@ std::vector<std::pair<int, int>> map::generateIslandPosition() {
     std::map<int, int> vecOfPairs{};
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> distrib(1, 106);
+    std::uniform_int_distribution<> distrib(1, 16);
     while (vecOfPairs.size() < MAX_ISLANDS) {
         vecOfPairs[distrib(gen)] = distrib(gen);
     }
@@ -34,4 +35,16 @@ std::vector<std::pair<int, int>> map::generateIslandPosition() {
     }
     returnPair.shrink_to_fit();
     return returnPair;
+}
+
+Island* map::getIsland(const Coordinates& position) const {
+    if (map_.size() != 0) {
+        auto it = std::find_if(map_.begin(), map_.end(), [&](std::shared_ptr<Island> const& search) {
+            return search->getPosition().getPositionX() == position.getPositionX() && search->getPosition().getPositionY() == position.getPositionY();
+        });
+        if(it != map_.end()) {
+            return map_[std::distance(map_.begin(),it)].get();
+        }
+    }
+    return nullptr;
 }
