f90b2e871f9a6509d5aed0890eca7f0b9da687e1
Katarzyna Duchna <77102726+gyrydymptum@users.noreply.github.com>
2021-06-29 12:21:00 +0200

    Revert "Shm"

--- shm/Ship.cpp
+++ shm/Ship.cpp
@@ -1,16 +1,26 @@
 #include "Ship.hpp"
-
-Ship::Ship()
-    : id_(-1) {}
+    
+Ship::Ship() 
+    : id_(-1)
+{}
 
 Ship::Ship(int capacity, int maxCrew, int speed, const std::string& name, size_t id, std::vector<std::unique_ptr<Cargo>> cargoVec)
-    : capacity_(capacity), maxCrew_(maxCrew), crew_(0), speed_(speed), name_(name), id_(id), cargoVec_(std::move(cargoVec)) {}
+    : capacity_(capacity)
+    , maxCrew_(maxCrew)
+    , crew_(0)
+    , speed_(speed)
+    , name_(name)
+    , id_(id)
+    , cargoVec_(std::move(cargoVec))
+{}
 
 Ship::Ship(int maxCrew, int speed, size_t id)
-    : Ship(0, maxCrew, speed, "", id, {}) {}
+    : Ship(0, maxCrew, speed, "", id, {})
+{}
 
-void Ship::setName(const std::string& name) {
-    name_ = name;
+
+void Ship::setName(const std::string& name) { 
+    name_ = name; 
 }
 
 Ship& Ship::operator-=(size_t num) {
@@ -23,59 +33,26 @@ Ship& Ship::operator+=(size_t num) {
     return *this;
 }
 
-size_t Ship::getCapacity() const {
-    return capacity_;
+size_t Ship::getCapacity() const { 
+    return capacity_; 
 }
 
-size_t Ship::getMaxCrew() const {
-    return maxCrew_;
+size_t Ship::getMaxCrew() const { 
+    return maxCrew_; 
 }
 
-size_t Ship::getSpeed() const {
-    return speed_;
+size_t Ship::getSpeed() const { 
+    return speed_; 
 }
 
-std::string Ship::getName() const {
-    return name_;
+std::string Ship::getName() const { 
+    return name_; 
 }
 
-size_t Ship::getId() const {
-    return id_;
+size_t Ship::getId() const { 
+    return id_; 
 }
 
-const std::vector<std::unique_ptr<Cargo>>& Ship::getCargoVec() const {
-    return cargoVec_;
-}
-
-void Ship::load(std::unique_ptr<Cargo> cargo) {
-    if (!cargo) {
-        return;
-    }
-    for (auto& el : cargoVec_) {
-        if (*cargo == *el) {
-            *el += cargo->getAmount();
-            return;
-        }
-    }
-    cargoVec_.push_back(move(cargo));
-}
-
-void Ship::unload(Cargo* cargo) {
-    if (!cargo) {
-        return;
-    }
-    for (auto& el : cargoVec_) {
-        if (*cargo == *el) {
-            if (el->getAmount() - cargo->getAmount() > 0) {
-                *el -= cargo->getAmount();
-                return;
-            }
-        }
-    }
-
-    cargoVec_.erase(std::remove_if(cargoVec_.begin(), cargoVec_.end(),
-                                   [&cargo](std::unique_ptr<Cargo>& cargoOnBoard) {
-                                       return *cargo == *cargoOnBoard;
-                                   }),
-                    cargoVec_.end());
+const std::vector<std::unique_ptr<Cargo>>& Ship::getCargoVec() const { 
+    return cargoVec_; 
 }
