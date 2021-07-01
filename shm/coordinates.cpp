2e06a68753adcf621b13c948d6133e47ce124034
Marcin$ <marcin.s10335@gmail.com>
2021-07-01 23:26:27 +0200

    Correct Coordiantes files

--- shm/coordinates.cpp
+++ shm/coordinates.cpp
@@ -1,9 +1,13 @@
 #include "coordinates.hpp"
 
-
 bool Coordinates::operator==(const Coordinates& other) const {
-    if (positionX_ == other.positionX_ and positionY_ == other.positionY_ ) {
-        return true;
-    }
-    return false;
+    return positionX_ == other.positionX_ && positionY_ == other.positionY_;
 }
+Coordinates::Coordinates()
+        : positionX_(0)
+        , positionY_(0)
+    {}
+Coordinates::Coordinates(int positionX, int positionY)
+        : positionX_(positionX)
+        , positionY_(positionY)
+    {}
