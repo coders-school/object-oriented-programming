b80f2bac5dcb5a2fa6a86188a9dc834220c998b8
BarTes8 <barbara.tessema@gmail.com>
2021-07-03 13:44:12 +0200

    changed Cargo += -= operator

--- shm/src/Cargo.cpp
+++ shm/src/Cargo.cpp
@@ -1,4 +1,5 @@
 #include "shm/inc/Cargo.hpp"
+#include "shm/inc/Player.hpp"
 
 #include <iostream>
 
@@ -9,16 +10,21 @@ Cargo::Cargo(const std::string& name, size_t amount, size_t basePrice)
 {}
 
 Cargo& Cargo::operator+=(const size_t amount) {
+    size_t availableSpace = Player::getAvailableSpace();
+    if (amount_ + amount > availableSpace) {
+        std::cerr << "You don't have enough space on the ship\0";
+        return *this;
+    }
     amount_ += amount;
     return *this;
 }
 
 Cargo& Cargo::operator-=(const size_t amount) {
-    if (amount <= amount_) {
-        amount_ -= amount;
-    } else {
-        std::cerr << "Amount of cargo can't be under 0\n";
+    if (cargo_ < cargo) {
+        std::cerr << "You don't have that much cargo on the ship\0";
+        return *this;
     }
+    cargo_ -= cargo;
     return *this;
 }
 
