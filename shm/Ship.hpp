991825eace8861d8139b98ff5969b88bac4b6ac9
Runer <runer53248@gmail.com>
2021-07-04 16:36:00 +0200

    Move implementation of load and unload in Ship.

--- shm/Ship.hpp
+++ shm/Ship.hpp
@@ -30,45 +30,6 @@ public:
     size_t getId() const;
     const std::vector<std::unique_ptr<Cargo>>& getCargoVec() const;
 
-    void load(std::unique_ptr<Cargo> cargo) {
-        if (!cargo) {
-            return;
-        }
-
-        Cargo* storedCargo = nullptr;
-        for(const auto& el : cargoVec_){
-            if(*cargo == *el){
-                storedCargo = el.get();
-                break;
-            }
-        }
-
-        if (!storedCargo) {
-            for (auto& el : cargoVec_) {
-                if (!el) {
-                    el = std::move(cargo);  //move cargo into empty cargo space
-                    return;
-                }
-            }
-            cargoVec_.push_back(std::move(cargo));  //move cargo into vector
-            return;
-        }
-        *storedCargo += cargo->getAmount();  //transfer amount into comperable cargo
-        //here cargo will be destroyed
-    }
-
-    void unload(const Cargo* const cargo) {  //ver A
-        if (!cargo) {
-            return;
-        }
-        for (auto& el : cargoVec_) {
-            if (el.get() == cargo) {  //equal pointers -> equal amount
-                el.reset();
-                //we leave empty cargo space in store here
-                cargoVec_.erase(std::remove(begin(cargoVec_), end(cargoVec_), nullptr), end(cargoVec_));
-                return;
-            }
-        }
-        throw std::logic_error("Store: Not my Cargo!");
-    }
+    void load(std::unique_ptr<Cargo> cargo);
+    void unload(const Cargo* const cargo);
 };
