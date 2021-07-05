f3b244b0ce23e3d7a8093002c4641405bd7d59b8
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-07-05 16:26:07 +0200

    Mark notify method as private (it is called by increment operator only)

--- shm/inc/Time.hpp
+++ shm/inc/Time.hpp
@@ -8,10 +8,11 @@ class Time {
 public:
     void addSubscriber(Subscriber* subscriber);
     void removeSubscriber(Subscriber* subscriber);
-    void notifySubscribers();
     Time& operator++();
 
 private:
     size_t elapsedTime_{};
     std::vector<Subscriber*> subscribers_;
+
+    void notifySubscribers();
 };
