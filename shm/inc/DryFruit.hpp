ce1bbb83b02bfda834147a1f7e78d8e6ecd68545
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-07-05 11:51:37 +0200

    Remove no more virtual declaration of Cargo getter & mark overridden methods origin in comments

--- shm/inc/DryFruit.hpp
+++ shm/inc/DryFruit.hpp
@@ -8,11 +8,11 @@ class DryFruit : public Fruit {
 public:
     DryFruit(const std::string& name, size_t amount, size_t basePrice);
     
-    //override from Fruit
-    DryFruit& operator--() override;
-
+    // override form Cargo
     std::size_t getPrice() const override;
-    std::string getName() const override { return name_; }
+    
+    // override from Fruit
+    DryFruit& operator--() override;
 
 private:
     size_t rottenDryFruit_ { DAYS_TO_ROTTEN_DRY_FRUIT };
