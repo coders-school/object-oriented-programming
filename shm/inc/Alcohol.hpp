c7ddb1e28d49740556ff379ca481af644ed17fae
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-07-05 11:41:51 +0200

    Remove definitions of Cargo getters as they are no more virtual & move other definitions to corresponding source

--- shm/inc/Alcohol.hpp
+++ shm/inc/Alcohol.hpp
@@ -10,21 +10,14 @@ class Alcohol : public Cargo {
 public:
     Alcohol(const std::string& name, size_t amount, size_t basePrice, size_t percentage);
 
-    //override Cargo
-    size_t getPrice() const override {
-        return basePrice_ * percentage_ / MAX_PERCENTAGE;
-    }
-    std::string getName() const override { return name_; }
-    size_t getAmount() const override { return amount_; }
-    size_t getBasePrice() const override { return basePrice_; }
-
+    // override from Cargo
+    size_t getPrice() const override;
     Cargo& operator+=(size_t amount) override;
     Cargo& operator-=(size_t amount) override;
     bool operator==(const Cargo& alcohol) const override;
 
-    size_t getPercentage() const { return percentage_; }
-
-    void setBasePrice(size_t basePrice) { basePrice_ = basePrice; } 
+    size_t getPercentage() const;
+    void setBasePrice(size_t basePrice);
 
 private:
     size_t percentage_;
