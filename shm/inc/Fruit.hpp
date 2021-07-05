b303d288f337311dcdb5f5e041f201561999cae3
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-07-05 11:34:47 +0200

    Remove definitions of Cargo getters as they are no more virtual & implement a default destructor

--- shm/inc/Fruit.hpp
+++ shm/inc/Fruit.hpp
@@ -4,24 +4,20 @@
 
 #include "shm/inc/Cargo.hpp"
 
-constexpr size_t DAYS_TO_ROTTEN { 10 };
+constexpr size_t DAYS_TO_ROTTEN{ 10 };
 
 class Fruit : public Cargo {
 public:
     Fruit(const std::string& name, size_t amount, size_t basePrice);
-    virtual ~Fruit();
+    virtual ~Fruit() = default;
 
-    // Override from Cargo class
-    std::string getName() const override { return name_; }
-    size_t getAmount() const override { return amount_; }
-    size_t getBasePrice() const override { return basePrice_; }
+    // override from Cargo class
     virtual size_t getPrice() const override;
-
     Cargo& operator+=(const size_t amount) override;
     Cargo& operator-=(const size_t amount) override;
     bool operator==(const Cargo& cargo) const override;
     virtual Fruit& operator--();
 
 protected:
-    size_t rottenTime_ { DAYS_TO_ROTTEN };
+    size_t rottenTime_{ DAYS_TO_ROTTEN };
 };
