f67638d4ef948f6d2ae4107ad6f601eb995b0ab6
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-07-05 11:31:08 +0200

    Add getters implementation common for each derived class

--- shm/src/Cargo.cpp
+++ shm/src/Cargo.cpp
@@ -5,3 +5,7 @@ Cargo::Cargo(const std::string& name, size_t amount, size_t basePrice)
     , amount_(amount)
     , basePrice_(basePrice)
 {}
+
+std::string Cargo::getName() const { return name_; };
+size_t Cargo::getAmount() const { return amount_; };
+size_t Cargo::getBasePrice() const { return basePrice_; };
