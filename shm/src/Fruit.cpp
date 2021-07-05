1d9e4ae1722f17611dfda442d1b21f9121d712e7
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-07-05 12:13:55 +0200

    Validate maximum amount of alcohol added & minimum subtracted

--- shm/src/Fruit.cpp
+++ shm/src/Fruit.cpp
@@ -1,5 +1,7 @@
 #include "shm/inc/Fruit.hpp"
 
+#include <stdexcept>
+
 Fruit::Fruit(const std::string& name, size_t amount, size_t basePrice)
    : Cargo(name, amount, basePrice)
 {}
@@ -11,17 +13,22 @@ Fruit& Fruit::operator--() {
     return *this;
 }
 
-std::size_t Fruit::getPrice() const {
+size_t Fruit::getPrice() const {
     return basePrice_ * (rottenTime_ / DAYS_TO_ROTTEN);
 }
 
 Cargo& Fruit::operator+=(const size_t amount) {
+    if (amount_ + amount > MAX_AMOUNT_OF_CARGO) {
+        throw std::out_of_range("Maximum amount of fruits reached!");
+    }
     amount_ += amount;
     return *this;
 }
 
 Cargo& Fruit::operator-=(const size_t amount) {
-    amount_ -= amount;
+    if (amount <= amount_) {
+        amount_ -= amount;
+    }
     return *this;
 }
 
