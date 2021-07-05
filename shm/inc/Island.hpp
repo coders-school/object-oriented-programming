71ac58f6321af6a1784e2a528df5e2c5690fc0a8
BarTes8 <barbara.tessema@gmail.com>
2021-07-05 22:36:22 +0200

    added distance method in class Coordinates

--- shm/inc/Island.hpp
+++ shm/inc/Island.hpp
@@ -9,6 +9,7 @@ public:
     public:
         Coordinates(size_t, size_t);
         bool operator==(const Coordinates&) const;
+        size_t distance(const Coordinates& lhs, const Coordinates& rhs);
     private:
         const size_t positionX_ = 0;
         const size_t positionY_ = 0;
