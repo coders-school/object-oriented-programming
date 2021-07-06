8de582378608f033cdc044bc576efaab8224ab87
Łukasz Meisnerowski <lukasz.meisnerowski@gmail.com>
2021-07-06 19:11:35 +0200

    Revert "Added new files coordinates.hpp/.cpp, modification of island.hpp - move coordinates class to new file, delete references, modification of operator=="
    This reverts commit 5b7353f6c9b5ba90c3c48a62fd628570087269e7.

--- shm/island.hpp
+++ shm/island.hpp
@@ -1,11 +1,30 @@
 #pragma once
-#include "coordinates.hpp"
 
 class Island {    
 public:
-    Island(Coordinates position)
-    : position_(position)
+    Island(int positionX, int positionY)
+    : position_(Coordinates(positionX, positionY))
     {}
+
+    class Coordinates {
+    public:    
+        Coordinates(int& positionX, int& positionY) 
+        : positionX_(positionX)
+        , positionY_(positionY)
+        {}
+
+        void setPositionX(int& );
+        int getPositionX() const;
+
+        void setPositionY(int& );
+        int getPositionY() const;
+    
+        bool operator== (const Coordinates& n_pos) const;
+
+    private:
+        int positionX_;
+        int positionY_;
+    };
 
     Coordinates getPosition() const;
 
