185f84b5a020f85978d027f334b1a6268bbec155
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-07-06 17:05:59 +0200

    Add declarations of game win and lost conditions

--- shm/inc/Game.hpp
+++ shm/inc/Game.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <cstddef>
 #include <stddef.h>
+#include <memory>
+
+class Player;
+class Time;
 
 class Game {
 public:
@@ -12,4 +17,10 @@ private:
     size_t money_ {};
     size_t game_days_ {};
     const size_t final_goal_ {};
+    size_t current_day_;
+    std::unique_ptr<Player> player_;
+    std::unique_ptr<Time> time_;
+
+    bool isGameWon() const;
+    bool isGameLost() const;
 };
