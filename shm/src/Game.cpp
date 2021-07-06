9376199ae8b4833f0c5d370bdbe7ba8a178f67b4
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-07-06 17:05:10 +0200

    Add game loop to play the game until win or end of days

--- shm/src/Game.cpp
+++ shm/src/Game.cpp
@@ -1,11 +1,25 @@
 #include "shm/inc/Game.hpp"
 
+#include "shm/inc/Player.hpp"
+#include "shm/inc/Time.hpp"
+#include <memory>
+
 Game::Game(size_t money, size_t game_days, size_t final_goal)
     : money_(money)
     , game_days_(game_days)
     , final_goal_(final_goal)
-{}
+    , time_(std::make_unique<Time>())
+{
+    current_day_ = time_->getElapsedTime();
+}
 
 void Game::startGame() {
-
+    while (game_days_ > current_day_) {
+        if (isGameWon()) {
+            // YOU WON!
+            return;
+        }
+        // CONTUNUE GAME
+    }
+    // YOU LOST!
 }
