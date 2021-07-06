9ed1f3cf799baab50ba7e74bf8adaa1cb72b1290
Łukasz Meisnerowski <lukasz.meisnerowski@gmail.com>
2021-07-06 19:32:45 +0200

    Added new files coordinates.hpp/.cpp, moved class coordinates to new file, delete references, modification of operator==

--- shm/island.cpp
+++ shm/island.cpp
@@ -1,27 +1,5 @@
 #include "island.hpp"
 
-void Island::Coordinates::setPositionX(int& positionX) {
-    positionX = positionX_;
-}
-int Island::Coordinates::getPositionX() const {
-    return positionX_;
-}
-
-void Island::Coordinates::setPositionY(int& positionY) {
-    positionY = positionY_;
-}
-int Island::Coordinates::getPositionY() const {
-    return positionY_;
-}
-
-bool Island::Coordinates::operator== (const Coordinates& n_pos) const {
-    // if ( positionX_ == n_pos.positionX_ && positionY_ == n_pos.positionY_) {
-    //     return true;
-    // }
-    // return false;
-    return positionX_ == n_pos.positionX_ && positionY_ == n_pos.positionY_;
-}
-
-Island::Coordinates Island::getPosition() const {
+Coordinates Island::getPosition() const {
     return position_;
 }
