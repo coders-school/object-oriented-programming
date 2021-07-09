6eb29ea813b7ce8d241006cb521ff378421d17d5
lukasz-kukulka <qqla83@gmail.com>
2021-07-09 02:42:03 +0100

    Complete convert fuction

--- shm/src/Store.cpp
+++ shm/src/Store.cpp
@@ -92,15 +92,22 @@ void Store::loadFromFile() {
 }
 
 void Store::convertDataFromFile(std::string lineFromFile) {
-    std::string name{};
-    size_t amountMin{};
-    size_t amountMax{};
-    size_t basePriceMin{};
-    size_t basePriceMax{};
-    size_t uniqueParameterMin{};
-    size_t uniqueParameterMax{};
-    for (auto const & ele : lineFromFile) {
-        
+    std::vector<std::string>dataToCreateItems(8, "");
+    // std::string itemType{};
+    // std::string name{};
+    // size_t amountMin{};
+    // size_t amountMax{};
+    // size_t basePriceMin{};
+    // size_t basePriceMax{};
+    // size_t uniqueParameterMin{};
+    // size_t uniqueParameterMax{};
+    size_t iterator{};
+    for (size_t i = 0; i < lineFromFile.size(); i++) {
+        if (lineFromFile[i] != ' ') {
+            dataToCreateItems[iterator] += lineFromFile[i];
+        } else {
+            iterator++;
+        }
     }
 }
 
