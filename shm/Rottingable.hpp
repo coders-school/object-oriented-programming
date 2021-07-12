3336c24f1987ec0366e531d09d42dded0b8850b6
Runer <runer53248@gmail.com>
2021-07-12 12:39:25 +0200

    Improve suffix. Add Rottinggable class. Add tests for DryFruit. Small fixes.

---
+++ shm/Rottingable.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+
+class Rottingable {
+public:
+    Rottingable(size_t freshTime, size_t maxFreshTime = 0)
+        : freshTime_(freshTime), maxFreshTime_(maxFreshTime) {
+        if (!maxFreshTime) {
+            maxFreshTime_ = freshTime_;
+        }
+    }
+    virtual ~Rottingable() = default;
+
+    bool operator==(const Rottingable& other) const {
+        return this->maxFreshTime_ == other.maxFreshTime_
+            and this->freshTime_ == other.freshTime_;
+    }
+
+    virtual Cargo& operator--() = 0;
+
+protected:
+    size_t maxFreshTime_;
+    size_t freshTime_;
+};
