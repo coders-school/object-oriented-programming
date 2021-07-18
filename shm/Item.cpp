98380b4968c659bf5d35bb7bee08d2f208249143
aniolm <maciej.aniol.programista@gmail.comgit config --global user.name Your>
2021-07-18 20:50:32 +0300

    Task 5 from OOP complete

---
+++ shm/Item.cpp
@@ -0,0 +1,4 @@
+#include "Item.hpp"
+
+Item::Item(const std::string& name, size_t amount, size_t basePrice, Rarity rarity)
+        : Cargo(name, amount, basePrice), rarity_(rarity){}
--- shm/main.cpp
+++ shm/main.cpp
@@ -1,5 +1,6 @@
-#include "Alcohol.hpp"
+#include "Item.hpp"
 #include "Fruit.hpp"
+#include "Rarity.hpp"
 #include <iostream>
 
 size_t operator+(size_t sum, const std::shared_ptr<Cargo> & cargo){
@@ -10,5 +11,7 @@ int main(){
     Fruit bananas("bananas",200, 40, 30 );
     ++bananas;
     std::cout<<bananas.gettimeInCargoRoom();
+    Item item("sword", 1, 40, Rarity::common);
+    std::cout<<static_cast<int>(item.getRarity());
     return 0;
 }
