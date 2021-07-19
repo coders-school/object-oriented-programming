e7d561428cddc87b2d05b31b173e52a05ef80dc8
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-07-19 17:59:28 +0200

    adding functionalities

--- shm/store.hpp
+++ shm/store.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+
 #include "cargo.hpp"
+#include "player.hpp"
 
 enum class Response {
     done,
@@ -10,5 +14,19 @@ enum class Response {
 };
 
 class Store {
+public:
+    Store() = default;
+
+    Response buy(std::shared_ptr<Cargo> cargo, size_t amount, std::shared_ptr<Player> player);
+    Response sell(std::shared_ptr<Cargo> cargo, size_t amount, std::shared_ptr<Player> player);
+
+    std::string getResponse(const Response);
+    std::shared_ptr<Cargo> getCargo(const  size_t ) const;
+
 private:
+    std::vector<std::shared_ptr<Cargo>> cargos_;
+    void generateCargo();
+    void randomizeCargo();
+    void load( std::shared_ptr<Cargo> cargo, size_t amount);
+    void unload( std::shared_ptr<Cargo> cargo, size_t amount);
 };
