47c25391a56fb9d8a28a4f74308be60559eb51b3
ms10335 <marcin.s10335@gmail.com>
2021-07-20 13:59:47 +0200

    Add search function

--- shm/store.cpp
+++ shm/store.cpp
@@ -1,30 +1,39 @@
 #include "store.hpp"
-#include "player.hpp"
 #include "cargo.hpp"
+#include "player.hpp"
 #include "ship.hpp"
 
-Store::Response Store::buy(Cargo *cargo, size_t amount, Player *player){
-    // free space on Player ship_
-    size_t getPlayerShipAmount = player->getAvailableSpace();
-    if (getPlayerShipAmount <= amount)
-    {
-        return Response::lack_of_space;
+bool Store::checkAvaiablityCargoInStore(std::shared_ptr<Cargo> searchCargo) {
+    auto result = find(cargosInStore.begin(), cargosInStore.end(), [&searchCargo](const Cargo& cargo){
+        return   cargo.getName() == searchCargo->getName(); 
+    }); 
+    if(result != cargosInStore.end()) {
+        return true;
     }
-    auto addCargoSize = cargo->getAmount() + amount;
-    if (getPlayerShipAmount <= addCargoSize)
-    {
+    return false;
+}
+
+Store::Response Store::buy(Cargo* cargo, size_t amount, Player* player) {
+    
+    
+    return Response::lack_of_cargo;  //// ????
+    
+    // free space on Player ship_
+    //check if add cargo amount is avaiable to add
+    size_t getPlayerShipAmount = player->getAvailableSpace();
+    size_t addCargoSize = cargo->getAmount() + amount;  
+    if (getPlayerShipAmount <= addCargoSize) {
         return Response::lack_of_space;
     }
 
     auto ship = player->getShip();
     ship->getCargo();
 
-    Response::lack_of_cargo; //// ????
+    
     Response::lack_of_money;
     Response::done;
 }
-Store::Response Store::sell(Cargo *, size_t amount, Player *)
-{
-
+Store::Response Store::sell(Cargo*, size_t amount, Player*) {
     return Response::done;
 }
+
--- shm/store.hpp
+++ shm/store.hpp
@@ -9,7 +9,14 @@ public:
                           lack_of_money,
                           lack_of_cargo,
                           lack_of_space };
-                          
+
     Response buy(Cargo*, size_t amount, Player*);
     Response sell(Cargo*, size_t amount, Player*);
+
+    void addToVecOfCargos(std::shared_ptr<Cargo>);
+    void dellFromVecOfCargos(std::shared_ptr<Cargo>);
+    bool checkAvaiablityCargoInStore(std::shared_ptr<Cargo>);
+
+private:
+    std::vector<std::shared_ptr<Cargo>> cargosInStore{};
 };
