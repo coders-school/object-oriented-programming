7a05ac4620112db59ecd462aeba442dd7d3fd3f4
kondorski <thegreatvaultofknowledge@gmail.com>
2021-07-21 21:15:31 +0200

    Delete binary, delete player and ship from Cargo::operator+=

--- shm/cargo.cpp
+++ shm/cargo.cpp
@@ -5,17 +5,8 @@
 
 Cargo::Cargo(std::string name, size_t amount, size_t basePrice) :name_(name), amount_(amount), basePrice_(basePrice){}
 
-/*size_t Player::gettingCargoAmount()
-{
-    std::vector<Cargo*> shipCargo = ship_->getCargo();
-}
-*/
 Cargo &Cargo::operator+=(size_t amount)
 {
-    auto ship = std::make_unique<Ship>(20, 1, 2, "titanic", 3);
-    Player player(std::move(ship), 1, 1);
-
-    //if(amount + amount > player.getCargo(0)->getAmount())
     amount_ += amount;
     return *this;
 }
